339088c35a165d9ecaebf1f0dc1788ee84c649cc
Toasterbirb <git@toasterbirb.com>
2023-04-17 18:14:26 +0300

    Add the first package to start experimenting with

---
+++ man-pages/seed.sh
@@ -0,0 +1,21 @@
+NAME="man-pages"
+VERSION="6.04"
+SOURCE="https://www.kernel.org/pub/linux/docs/man-pages/man-pages-${VERSION}.tar.xz"
+CHECKSUM="bbce1456edfa75a66c189cc594af6c4d"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xvf $DISTFILES/man-pages-${VERSION}.tar.xz
+}
+
+_build()
+{
+	printf ""
+}
+
+_install()
+{
+	make prefix=$FAKEROOT/usr install
+}
