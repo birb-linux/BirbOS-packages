c8f9a279a767f7ba08e50c5a8f73e200fe0a3170
Toasterbirb <git@toasterbirb.com>
2023-04-17 22:09:13 +0300

    Add a package description

--- man-pages/seed.sh
+++ man-pages/seed.sh
@@ -1,4 +1,5 @@
 NAME="man-pages"
+DESC="The Man-pages package contains over 2,400 man pages."
 VERSION="6.04"
 SOURCE="https://www.kernel.org/pub/linux/docs/man-pages/man-pages-${VERSION}.tar.xz"
 CHECKSUM="bbce1456edfa75a66c189cc594af6c4d"
