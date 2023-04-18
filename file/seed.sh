cfecf327688cdab10ab3c8af6011ae48173338da
Toasterbirb <git@toasterbirb.com>
2023-04-18 15:15:54 +0300

    Package file

---
+++ file/seed.sh
@@ -0,0 +1,62 @@
+NAME="file"
+DESC="Utility for determining the type of a given file or files"
+VERSION="5.44"
+SOURCE="https://astron.com/pub/file/file-${VERSION}.tar.gz"
+CHECKSUM="a60d586d49d015d842b9294864a89c7a"
+DEPS=""
+FLAGS="32bit"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_build32()
+{
+	make distclean
+
+	CC="gcc -m32" ./configure \
+		--prefix=$FAKEROOT/$NAME/usr \
+		--libdir=/usr/lib32   \
+		--host=i686-pc-linux-gnu
+
+	make -j$(nproc)
+}
+
+_install32()
+{
+	make DESTDIR=$PWD/DESTDIR install
+	cp -Rv DESTDIR/usr/lib32/* $FAKEROOT/$NAME/usr/lib32
+	rm -rf DESTDIR
+}
+
+_buildx32()
+{
+	make distclean
+
+	CC="gcc -mx32" ./configure \
+		--prefix=$FAKEROOT/$NAME/usr \
+		--libdir=/usr/libx32   \
+		--host=x86_64-pc-linux-gnux32
+
+	make -j$(nproc)
+}
+
+_installx32()
+{
+	make DESTDIR=$PWD/DESTDIR install
+	cp -Rv DESTDIR/usr/libx32/* $FAKEROOT/$NAME/usr/libx32
+	rm -rf DESTDIR
+}
