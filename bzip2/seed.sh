ec494264d84182870c83cf481a420bdc8d686c73
Toasterbirb <git@toasterbirb.com>
2023-04-18 14:40:08 +0300

    Add 32bit libs to bzip2

--- bzip2/seed.sh
+++ bzip2/seed.sh
@@ -4,7 +4,7 @@ VERSION="1.0.8"
 SOURCE="https://www.sourceware.org/pub/bzip2/bzip2-${VERSION}.tar.gz"
 CHECKSUM="67e051268d0c475ea773822f7500d0e5"
 DEPS=""
-FLAGS=""
+FLAGS="32bit"
 
 _setup()
 {
@@ -36,13 +36,47 @@ _install()
 	cp -av libbz2.so.* $FAKEROOT/$NAME/usr/lib
 	ln -sv libbz2.so.1.0.8 $FAKEROOT/$NAME/usr/lib/libbz2.so
 
-	cp -v bzip2-shared /usr/bin/bzip2
+	cp -v bzip2-shared $FAKEROOT/$NAME/usr/bin/bzip2
 	for i in $FAKEROOT/$NAME/usr/bin/{bzcat,bunzip2}; do
 	  ln -sfv bzip2 $i
 	done
 
 	# Remove a useless static library
 	rm -fv /usr/lib/libbz2.a
+}
 
-	echo "Skipping 32bit libraries for this package"
+_build32()
+{
+	make clean
+
+	sed -e "s/^CC=.*/CC=gcc -m32/" -i Makefile{,-libbz2_so}
+	make -f Makefile-libbz2_so
+	make libbz2.a
+}
+
+_install32()
+{
+	install -Dm755 libbz2.so.1.0.8 $FAKEROOT/$NAME/usr/lib32/libbz2.so.1.0.8
+	ln -sf libbz2.so.1.0.8 $FAKEROOT/$NAME/usr/lib32/libbz2.so
+	ln -sf libbz2.so.1.0.8 $FAKEROOT/$NAME/usr/lib32/libbz2.so.1
+	ln -sf libbz2.so.1.0.8 $FAKEROOT/$NAME/usr/lib32/libbz2.so.1.0
+	install -Dm644 libbz2.a $FAKEROOT/$NAME/usr/lib32/libbz2.a
+}
+
+_buildx32()
+{
+	make clean
+
+	sed -e "s/^CC=.*/CC=gcc -mx32/" -i Makefile{,-libbz2_so}
+	make -f Makefile-libbz2_so
+	make libbz2.a
+}
+
+_installx32()
+{
+	install -Dm755 libbz2.so.1.0.8 $FAKEROOT/$NAME/usr/libx32/libbz2.so.1.0.8
+	ln -sf libbz2.so.1.0.8 $FAKEROOT/$NAME/usr/libx32/libbz2.so
+	ln -sf libbz2.so.1.0.8 $FAKEROOT/$NAME/usr/libx32/libbz2.so.1
+	ln -sf libbz2.so.1.0.8 $FAKEROOT/$NAME/usr/libx32/libbz2.so.1.0
+	install -Dm644 libbz2.a $FAKEROOT/$NAME/usr/libx32/libbz2.a
 }
