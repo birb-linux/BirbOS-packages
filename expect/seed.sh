c18ec223b7e121711ac18314f11e0d7eea7e2f9f
Toasterbirb <git@toasterbirb.com>
2023-04-20 01:14:21 +0300

    Package expect

---
+++ expect/seed.sh
@@ -0,0 +1,33 @@
+NAME="expect"
+DESC="Tools for automating, via scripted dialogues and interactive applications"
+VERSION="5.45.4"
+SOURCE="https://prdownloads.sourceforge.net/expect/expect${VERSION}.tar.gz"
+CHECKSUM=""
+DEPS="tcl"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr \
+            --with-tcl=/usr/lib     \
+            --enable-shared         \
+            --mandir=$FAKEROOT/$NAME/usr/share/man \
+            --with-tclinclude=/usr/include
+
+	make -j$(nproc)
+
+	# Run tests
+	make -j$(nproc) test
+}
+
+_install()
+{
+	make install
+	ln -svf expect5.45.4/libexpect5.45.4.so $FAKEROOT/$NAME/usr/lib
+}
