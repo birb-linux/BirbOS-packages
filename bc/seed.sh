0663a864ebbfc3a34d11bad79badb7a1aa83afd2
Toasterbirb <git@toasterbirb.com>
2023-04-19 21:12:53 +0300

    Add a missing checksum

--- bc/seed.sh
+++ bc/seed.sh
@@ -2,7 +2,7 @@ NAME="bc"
 DESC="Arbitrary precision numeric processing language"
 VERSION="6.5.0"
 SOURCE="https://github.com/gavinhoward/bc/releases/download/${VERSION}/bc-${VERSION}.tar.xz"
-CHECKSUM=""
+CHECKSUM="e4757f48340dfe74af0d5be7ea56f7c7"
 DEPS=""
 FLAGS=""
 
