f11930857e5a3e2d29c7349ab93f432b96ef8eb0
Toasterbirb <git@toasterbirb.com>
2023-04-20 13:18:40 +0300

    Package mpc

---
+++ mpc/seed.sh
@@ -0,0 +1,32 @@
+NAME="mpc"
+DESC="Library for the airthmetic of complex numbers with arbitrarily high precision and correct rounding of the result"
+VERSION="1.3.1"
+SOURCE="https://ftp.gnu.org/gnu/mpc/mpc-${VERSION}.tar.gz"
+CHECKSUM="5c9bc658c9fd0f940e8e3e0f09530c62"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr \
+            --disable-static \
+            --docdir=/usr/share/doc/mpc-1.3.1
+
+	make -j$(nproc)
+	make html
+
+	# Run tests
+	make -j$(nproc) check
+}
+
+_install()
+{
+	make install
+	make install-html
+}
