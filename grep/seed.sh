f09c2c73b79241cc4b4fcce79df2b5b12a3c0bf5
Toasterbirb <git@toasterbirb.com>
2023-04-21 00:16:48 +0300

    Package grep

---
+++ grep/seed.sh
@@ -0,0 +1,34 @@
+NAME="grep"
+DESC="Programs for searching through the contents of a file"
+VERSION="3.10"
+SOURCE="https://ftp.gnu.org/gnu/grep/grep-${VERSION}.tar.xz"
+CHECKSUM="ab3f063ad4596b7d094fb5f66cf327d6"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Remove some warnings about using egrep and fgrep, because these make
+	# some tests fail
+	sed -i "s/echo/#echo/" src/egrep.sh
+
+	./configure --prefix=$FAKEROOT/$NAME/usr
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j$(nproc) check
+}
