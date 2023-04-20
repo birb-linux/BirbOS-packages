77a68825ead8170dca1a5a90faabe841fdb345c4
Toasterbirb <git@toasterbirb.com>
2023-04-20 16:20:58 +0300

    Package shadow

---
+++ shadow/seed.sh
@@ -0,0 +1,54 @@
+NAME="shadow"
+DESC="Programs for handling passwords in a secure way"
+VERSION="4.13"
+SOURCE="https://github.com/shadow-maint/shadow/releases/download/${VERSION}/shadow-${VERSION}.tar.xz"
+CHECKSUM="b1ab01b5462ddcf43588374d57bec123"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Don't install the "groups" program and its man pages, because Coreutils has
+	# a better version of these. This also prevents the installation of duplicate
+	# man pages that the man-pages package already provides
+	sed -i 's/groups$(EXEEXT) //' src/Makefile.in
+	find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
+	find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
+	find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
+
+	# 1. Change the algorithm to SHA-512 and increase the iteration amount
+	# 2. Change the mail location to /var/mail
+	# 3. Remove bin and sbin from PATH
+	sed -e 's:#ENCRYPT_METHOD DES:ENCRYPT_METHOD SHA512:' \
+		-e 's@#\(SHA_CRYPT_..._ROUNDS 5000\)@\100@'       \
+		-e 's:/var/spool/mail:/var/mail:'                 \
+		-e '/PATH=/{s@/sbin:@@;s@/bin:@@}'                \
+		-i etc/login.defs
+
+	touch /usr/bin/passwd
+	./configure --sysconfdir=/etc \
+				--disable-static  \
+				--with-group-name-max-length=32
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make exec_prefix=$FAKEROOT/$NAME/usr install
+	make -C man install-man
+
+	# Enable shadowed passwords
+	pwconv
+
+	# Enable shadowed group passwords
+	grpconv
+
+	echo "Remember to set a password for the root user with 'passwd root' command"
+}
