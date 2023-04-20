a82bc915a929ad50e4fccd9b8c859382cb1088b3
Toasterbirb <git@toasterbirb.com>
2023-04-21 02:16:30 +0300

    Package intltool

---
+++ intltool/seed.sh
@@ -0,0 +1,35 @@
+NAME="intltool"
+DESC="Internationalization tool used for extracting translatable strings from source files"
+VERSION="0.51.0"
+SOURCE="https://launchpad.net/intltool/trunk/${VERSION}/+download/intltool-${VERSION}.tar.gz"
+CHECKSUM="12e517cac2b57a0121cda351570f1e63"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Fix a perl warning
+	sed -i 's:\\\${:\\\$\\{:' intltool-update.in
+
+	./configure --prefix=$FAKEROOT/$NAME/usr
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+	mkdir -p $FAKEROOT/$NAME/usr/share/doc/intltool-${VERSION}
+	install -v -Dm644 doc/I18N-HOWTO $FAKEROOT/$NAME/usr/share/doc/intltool-${VERSION}/I18N-HOWTO
+}
+
+_test()
+{
+	make -j$(nproc) check
+}
