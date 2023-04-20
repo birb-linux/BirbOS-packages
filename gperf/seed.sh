87cf03f4936584e8686602a75820956cd48d79fe
Toasterbirb <git@toasterbirb.com>
2023-04-21 00:50:45 +0300

    Package gperf

---
+++ gperf/seed.sh
@@ -0,0 +1,30 @@
+NAME="gperf"
+DESC="Program for generating a perfect hash function from a key set"
+VERSION="3.1"
+SOURCE="https://ftp.gnu.org/gnu/gperf/gperf-${VERSION}.tar.gz"
+CHECKSUM="9e251c0a618ad0824b51117d5d9db87e"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr --docdir=/usr/share/doc/gperf-${VERSION}
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j1 check
+}
