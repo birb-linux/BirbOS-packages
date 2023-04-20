0b925c793a9aa8605313c9516403211543886f31
Toasterbirb <git@toasterbirb.com>
2023-04-20 11:53:48 +0300

    Package dejagnu

---
+++ dejagnu/seed.sh
@@ -0,0 +1,33 @@
+NAME="dejagnu"
+DESC="Framework for running test suites on GNU tools"
+VERSION="1.6.3"
+SOURCE="https://ftp.gnu.org/gnu/dejagnu/dejagnu-${VERSION}.tar.gz"
+CHECKSUM="68c5208c58236eba447d7d6d1326b821"
+DEPS="expect"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir -v build
+	cd       build
+
+	../configure --prefix=$FAKEROOT/$NAME/usr
+	makeinfo --html --no-split -o doc/dejagnu.html ../doc/dejagnu.texi
+	makeinfo --plaintext       -o doc/dejagnu.txt  ../doc/dejagnu.texi
+}
+
+_install()
+{
+	make install
+	install -v -dm755  $FAKEROOT/$NAME/usr/share/doc/dejagnu-1.6.3
+	install -v -m644   doc/dejagnu.{html,txt} $FAKEROOT/$NAME/usr/share/doc/dejagnu-1.6.3
+
+	# Run tests
+	make -j$(nproc) check
+}
