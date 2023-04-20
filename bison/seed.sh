aed5356dbbbd3e7ff738a4cd9528dfd0af4c2e58
Toasterbirb <git@toasterbirb.com>
2023-04-21 00:03:53 +0300

    Package bison

---
+++ bison/seed.sh
@@ -0,0 +1,30 @@
+NAME="bison"
+DESC="Parser generator"
+VERSION="3.8.2"
+SOURCE="https://ftp.gnu.org/gnu/bison/bison-${VERSION}.tar.xz"
+CHECKSUM="c28f119f405a2304ff0a7ccdcc629713"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr --docdir=/usr/share/doc/bison-${VERSION}
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j$(nproc) check
+}
