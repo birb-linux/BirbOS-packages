b2e355f05b4a7ebc49f7e86c6b0aa0ca157d8d10
Toasterbirb <git@toasterbirb.com>
2023-04-20 12:47:50 +0300

    Package binutils

---
+++ binutils/seed.sh
@@ -0,0 +1,47 @@
+NAME="binutils"
+DESC="Linker, an assembler and other tools for handling object files"
+VERSION="2.40"
+SOURCE="https://sourceware.org/pub/binutils/releases/binutils-${VERSION}.tar.xz"
+CHECKSUM="007b59bd908a737c06e5a8d3d2c737eb"
+DEPS="zlib"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir -v build
+	cd       build
+
+	../configure --prefix=$FAKEROOT/$NAME/usr \
+             --sysconfdir=/etc   \
+             --enable-gold       \
+             --enable-ld=default \
+             --enable-plugins    \
+             --enable-shared     \
+             --disable-werror    \
+             --enable-64-bit-bfd \
+             --with-system-zlib  \
+             --enable-multilib
+
+	make -j$(nproc) tooldir=$FAKEROOT/$NAME/usr
+
+	# Run tests
+	make -j$(nproc) -k check
+
+	echo "List of failed checks"
+	grep '^FAIL:' $(find -name '*.log')
+}
+
+_install()
+{
+	make tooldir=$FAKEROOT/$NAME/usr install
+
+	# remove useless static libraries and an empty man page
+	rm -fv $FAKEROOT/$NAME/usr/lib/lib{bfd,ctf,ctf-nobfd,sframe,opcodes}.a
+	rm -fv $FAKEROOT/$NAME/usr/share/man/man1/{gprofng,gp-*}.1
+}
