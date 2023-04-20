f32c25062b4ec5fda97a0f939df1c35747558c6d
Toasterbirb <git@toasterbirb.com>
2023-04-20 19:56:00 +0300

    Package pkg-config

---
+++ pkg-config/seed.sh
@@ -0,0 +1,38 @@
+NAME="pkg-config"
+DESC="Tool for passing the include path and/or library paths to build tools"
+VERSION="0.29.2"
+SOURCE="https://pkg-config.freedesktop.org/releases/pkg-config-${VERSION}.tar.gz"
+CHECKSUM="f6e931e319531b736fadc017f470e68a"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr \
+            --with-internal-glib       \
+            --disable-host-tool        \
+            --docdir=/usr/share/doc/pkg-config-${VERSION}
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+# If the package has any tests, run them here
+# To enable this function, specify the 'test' flag
+#
+# Also, birb needs to be run with special arguments to run tests
+# when installing packages
+_test()
+{
+	make -j$(nproc) check
+}
