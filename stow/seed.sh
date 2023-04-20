f1d7b68d9c283b02f7ea478e0d4a12b20fc5a061
Toasterbirb <git@toasterbirb.com>
2023-04-21 02:06:09 +0300

    Package stow

---
+++ stow/seed.sh
@@ -0,0 +1,28 @@
+NAME="stow"
+DESC="Symlink farm manager"
+VERSION="2.3.1"
+SOURCE="https://ftp.gnu.org/gnu/stow/stow-${VERSION}.tar.gz"
+CHECKSUM="4dfd82b93bb6702d018b1d57e498a74d"
+DEPS="perl"
+FLAGS="test important"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr
+}
+
+_install()
+{
+	make -j$(nproc)
+}
+
+_test()
+{
+	make test
+}
