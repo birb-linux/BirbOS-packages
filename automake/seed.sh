b096cec1019540a94b53c3ce6ce5a6ed3e1096c8
Toasterbirb <git@toasterbirb.com>
2023-04-21 13:54:05 +0300

    Package automake

---
+++ automake/seed.sh
@@ -0,0 +1,30 @@
+NAME="automake"
+DESC="Programs for generating Makefiles for use with autoconf"
+VERSION="1.16.5"
+SOURCE="https://ftp.gnu.org/gnu/automake/automake-${VERSION}.tar.xz"
+CHECKSUM="4017e96f89fca45ca946f1c5db6be714"
+DEPS=""
+FLAGS="test important"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr --docdir=/usr/share/doc/automake-${VERSION}
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j$(nproc) check
+}
