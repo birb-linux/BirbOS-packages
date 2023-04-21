b7f6491aba186af4be16d794937a3ae50af9d282
Toasterbirb <git@toasterbirb.com>
2023-04-21 14:21:29 +0300

    Fix 32bit installation path

--- openssl/seed.sh
+++ openssl/seed.sh
@@ -60,6 +60,6 @@ _build32()
 _install32()
 {
 	make DESTDIR=$PWD/DESTDIR install
-	cp -Rv DESTDIR/usr/lib32/* $FAKEROOT/$NAME/usr/lib32
+	cp -Rv DESTDIR/$FAKEROOT/$NAME/usr/lib32/* $FAKEROOT/$NAME/usr/lib32
 	rm -rf DESTDIR
 }
