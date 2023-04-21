481186254c2f95a27d9c77857c09435d6b918e36
Toasterbirb <git@toasterbirb.com>
2023-04-21 13:40:08 +0300

    Package autoconf

---
+++ autoconf/seed.sh
@@ -0,0 +1,35 @@
+NAME="autoconf"
+DESC="Programs for producing shell scripts that can automatically configure source code"
+VERSION="2.71"
+SOURCE="https://ftp.gnu.org/gnu/autoconf/autoconf-${VERSION}.tar.xz"
+CHECKSUM="12cfa1687ffa2606337efe1a64416106"
+DEPS=""
+FLAGS="important test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Fix issues with new bash versions
+	sed -e 's/SECONDS|/&SHLVL|/'               \
+		-e '/BASH_ARGV=/a\        /^SHLVL=/ d' \
+		-i.orig tests/local.at
+
+	./configure --prefix=$FAKEROOT/$NAME/usr
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j$(nproc) check
+}
