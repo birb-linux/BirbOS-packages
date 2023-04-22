0c69d2363204a2c4570832da44f5bb3186777553
Toasterbirb <git@toasterbirb.com>
2023-04-22 15:30:45 +0300

    Package flit-core

---
+++ flit-core/seed.sh
@@ -0,0 +1,23 @@
+NAME="flit-core"
+DESC="Distribution-building parts of Flit (a packaging tool for simple Python modules)"
+VERSION="3.8.0"
+SOURCE="https://pypi.org/packages/source/f/flit-core/flit_core-${VERSION}.tar.gz"
+CHECKSUM="7c41da13273f7787709a24f74e0f5a99"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd flit_core-${VERSION}
+}
+
+_build()
+{
+	pip3 wheel -w $FAKEROOT/$NAME/dist --no-build-isolation --no-deps $PWD
+}
+
+_install()
+{
+	pip3 install --no-index --no-user --find-links $FAKEROOT/$NAME/dist flit_core
+}
