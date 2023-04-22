59ae2093decca19c4fa505e56a887da624ddea20
Toasterbirb <git@toasterbirb.com>
2023-04-23 01:30:57 +0300

    Package p11-kit

---
+++ p11-kit/seed.sh
@@ -0,0 +1,49 @@
+NAME="p11-kit"
+DESC="The p11-kit package provides a way to load and enumerate PKCS #11 modules"
+VERSION="0.24.1"
+SOURCE="https://github.com/p11-glue/p11-kit/releases/download/${VERSION}/p11-kit-${VERSION}.tar.xz"
+CHECKSUM="67b2539bdca6b4bedaeecc12864d2796"
+DEPS="libtasn1"
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Prepare the distribution specific anchor hook
+	sed '20,$ d' -i trust/trust-extract-compat
+	cat >> trust/trust-extract-compat << "EOF"
+# Copy existing anchor modifications to /etc/ssl/local
+/usr/libexec/make-ca/copy-trust-modifications
+
+# Update trust stores
+/usr/sbin/make-ca -r
+EOF
+
+
+	mkdir p11-build
+	cd    p11-build
+
+	meson setup .. \
+		  --prefix=$FAKEROOT/$NAME/usr \
+		  --buildtype=release \
+		  -Dtrust_paths=/etc/pki/anchors
+
+	ninja
+}
+
+_install()
+{
+	ninja install
+	ln -sfv $FAKEROOT/$NAME/usr/libexec/p11-kit/trust-extract-compat \
+			$FAKEROOT/$NAME/usr/bin/update-ca-certificates
+}
+
+_test()
+{
+	ninja test
+}
