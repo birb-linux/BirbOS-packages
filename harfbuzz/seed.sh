d52704546a788ce96fc849f5a02db4fa2d44f6f9
Toasterbirb <git@toasterbirb.com>
2023-04-22 20:43:29 +0300

    Package harfbuzz (and remove freetype2 in favor of simply freetype)

---
+++ harfbuzz/seed.sh
@@ -0,0 +1,30 @@
+NAME="harfbuzz"
+DESC="OpenType text shaping engine"
+VERSION="7.1.0"
+SOURCE="https://github.com/harfbuzz/harfbuzz/releases/download/${VERSION}/harfbuzz-${VERSION}.tar.xz"
+CHECKSUM="761fe38814b74eb6d66f450051604a1f"
+DEPS="glib graphite2 icu"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build &&
+	cd    build &&
+
+	meson setup ..            \
+		  --prefix=$FAKEROOT/$NAME/usr \
+		  --buildtype=release \
+		  -Dgraphite2=enabled &&
+	ninja
+}
+
+_install()
+{
+	ninja install
+}
