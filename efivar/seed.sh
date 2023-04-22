5897faf1547b04b87a2d3f0e4d096091fd6be17b
Toasterbirb <git@toasterbirb.com>
2023-04-22 19:58:34 +0300

    Package efivar

---
+++ efivar/seed.sh
@@ -0,0 +1,27 @@
+NAME="efivar"
+DESC="Tools and libraries for manipulating EFI variables"
+VERSION="38"
+SOURCE="https://github.com/rhboot/efivar/releases/download/${VERSION}/efivar-${VERSION}.tar.bz2"
+CHECKSUM="243fdbc48440212695cb9c6e6fd0f44f"
+DEPS="mandoc"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Fix an issue in the Makefile that would cause the package
+	# to get rebuilt during installation
+	sed '/prep :/a\\ttouch prep' -i src/Makefile
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install DESTDIR=$FAKEROOT/$NAME/usr LIBDIR=$FAKEROOT/$NAME/usr/lib
+}
