5519a2a687886bbc22360cd2c1fb81a79365ed0b
Toasterbirb <git@toasterbirb.com>
2023-04-22 20:09:49 +0300

    Package efibootmgr

---
+++ efibootmgr/seed.sh
@@ -0,0 +1,23 @@
+NAME="efibootmgr"
+DESC="Tools and libraries for manipulating EFI variables"
+VERSION="18"
+SOURCE="https://github.com/rhboot/efibootmgr/archive/${VERSION}/efibootmgr-${VERSION}.tar.gz"
+CHECKSUM="e170147da25e1d5f72721ffc46fe4e06"
+DEPS="efivar popt"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	make -j$(nproc) EFIDIR=BIRBOS EFI_LOADER=grubx64.efi
+}
+
+_install()
+{
+	make install DESTDIR=$FAKEROOT/$NAME EFIDIR=BIRBOS
+}
