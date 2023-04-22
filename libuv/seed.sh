e3fb47000dde45c73417d26c9833bcf812894715
Toasterbirb <git@toasterbirb.com>
2023-04-23 02:48:04 +0300

    Package libuv

---
+++ libuv/seed.sh
@@ -0,0 +1,26 @@
+NAME="libuv"
+DESC="Multi-platform support library with a focus on asynchronous I/O"
+VERSION="1.44.2"
+SOURCE="https://dist.libuv.org/dist/v${VERSION}/libuv-v${VERSION}.tar.gz"
+CHECKSUM="0addbddcb1098e2c1c99f03f0108e1e7"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	sh autogen.sh
+	./configure --prefix=$FAKEROOT/$NAME/usr --disable-static
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
