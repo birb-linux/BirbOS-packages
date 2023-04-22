ed680d698859bf3e19d157532236adfc061a598c
Toasterbirb <git@toasterbirb.com>
2023-04-23 01:11:56 +0300

    Package graphite2

---
+++ graphite2/seed.sh
@@ -0,0 +1,37 @@
+NAME="graphite2"
+DESC="Rendering engine for graphite fonts"
+VERSION="1.3.14"
+SOURCE="https://github.com/silnrsi/graphite/releases/download/${VERSION}/graphite2-${VERSION}.tgz"
+CHECKSUM="1bccb985a7da01092bfb53bb5041e836"
+DEPS="cmake"
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Remove tests that are known to fail without the FontTools
+	# python 3 module
+	sed -i '/cmptest/d' tests/CMakeLists.txt
+
+	mkdir build
+	cd    build
+
+	cmake -DCMAKE_INSTALL_PREFIX=$FAKEROOT/$NAME/usr ..
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j$(nproc) test
+}
