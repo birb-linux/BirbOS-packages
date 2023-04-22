b7f86273598fa672cdd6fc24ed0d8ba3198fd2fa
Toasterbirb <git@toasterbirb.com>
2023-04-22 15:54:25 +0300

    Package wheel

---
+++ wheel/seed.sh
@@ -0,0 +1,23 @@
+NAME="wheel"
+DESC="Python library that is the reference implementation of the Python wheel packaging standard"
+VERSION="0.40.0"
+SOURCE="https://pypi.org/packages/source/w/wheel/wheel-${VERSION}.tar.gz"
+CHECKSUM="ec5004c46d1905da98bb5bc1a10ddd21"
+DEPS="python3"
+FLAGS="python"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	pip3 wheel -w $FAKEROOT/$NAME/dist --no-build-isolation --no-deps $PWD
+}
+
+_install()
+{
+	pip3 install --no-index --find-links=$FAKEROOT/$NAME/dist wheel
+}
