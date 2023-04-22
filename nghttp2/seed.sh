02b710868a34b772db294e3db63e547346d12998
Toasterbirb <git@toasterbirb.com>
2023-04-23 02:58:09 +0300

    Package nghttp2

---
+++ nghttp2/seed.sh
@@ -0,0 +1,28 @@
+NAME="nghttp2"
+DESC="Implementation of HTTP/2 and its header compression algorithm, HPACK"
+VERSION="1.52.0"
+SOURCE="https://github.com/nghttp2/nghttp2/releases/download/v${VERSION}/nghttp2-${VERSION}.tar.xz"
+CHECKSUM="30cab20160abb7981c713544d31e2d60"
+DEPS="libxml2"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr \
+            --disable-static  \
+            --enable-lib-only \
+            --docdir=$FAKEROOT/$NAME/usr/share/doc/nghttp2-${VERSION}
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
