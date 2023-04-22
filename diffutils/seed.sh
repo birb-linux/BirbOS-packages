585e694943271b8d99f75ab9491af1887410e274
Toasterbirb <git@toasterbirb.com>
2023-04-22 19:28:58 +0300

    Package diffutils

---
+++ diffutils/seed.sh
@@ -0,0 +1,30 @@
+NAME="diffutils"
+DESC="Programs for finding differences between files and directories"
+VERSION="3.9"
+SOURCE="https://ftp.gnu.org/gnu/diffutils/diffutils-${VERSION}.tar.xz"
+CHECKSUM="cf0a65266058bf22fe3feb69e57ffc5b"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j$(nproc) check
+}
