846c0a2f19c02275728e34deba9d4d3b53b044c4
Toasterbirb <git@toasterbirb.com>
2023-04-22 19:21:25 +0300

    Package check

---
+++ check/seed.sh
@@ -0,0 +1,29 @@
+NAME="check"
+DESC="Unit testing framework for C"
+VERSION="0.15.2"
+SOURCE="https://github.com/libcheck/check/releases/download/${VERSION}/check-${VERSION}.tar.gz"
+CHECKSUM="50fcafcecde5a380415b12e9c574e0b2"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr --disable-static
+	make -j$(nproc)
+}
+
+_install()
+{
+	make docdir=$FAKEROOT/$NAME/usr/share/doc/check-${VERSION} install
+}
+
+_test()
+{
+	make -j$(nproc) check
+}
