05f41e09b777d03f27627d32b9e849b05ff5da5e
Toasterbirb <git@toasterbirb.com>
2023-04-23 05:08:44 +0300

    Package gzip

---
+++ gzip/seed.sh
@@ -0,0 +1,30 @@
+NAME="gzip"
+DESC="Programs for compressing and decompressing files"
+VERSION="1.12"
+SOURCE="https://ftp.gnu.org/gnu/gzip/gzip-${VERSION}.tar.xz"
+CHECKSUM="9608e4ac5f061b2a6479dc44e917a5db"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j$(nproc) check
+}
