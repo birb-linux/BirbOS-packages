614281e80d88de0729b6950ca57bc401898cb06f
Toasterbirb <git@toasterbirb.com>
2023-04-23 03:47:17 +0300

    Package sqlite

---
+++ sqlite/seed.sh
@@ -0,0 +1,34 @@
+NAME="sqlite"
+DESC="Self-contained, serverless, zero-configuration, transactional SQL database engine"
+VERSION="3400100"
+SOURCE="https://sqlite.org/2022/sqlite-autoconf-${VERSION}.tar.gz"
+CHECKSUM="42175b1a1d23529cb133bbd2b5900afd"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr \
+            --disable-static  \
+            --enable-fts5     \
+            CPPFLAGS="-DSQLITE_ENABLE_FTS3=1            \
+                      -DSQLITE_ENABLE_FTS4=1            \
+                      -DSQLITE_ENABLE_COLUMN_METADATA=1 \
+                      -DSQLITE_ENABLE_UNLOCK_NOTIFY=1   \
+                      -DSQLITE_ENABLE_DBSTAT_VTAB=1     \
+                      -DSQLITE_SECURE_DELETE=1          \
+                      -DSQLITE_ENABLE_FTS3_TOKENIZER=1"
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
