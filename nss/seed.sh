4f00c5ed7b590d6698fd4daaaa56caec8f294d45
Toasterbirb <git@toasterbirb.com>
2023-04-23 16:43:09 +0300

    Remove the p11-kit dependency

--- nss/seed.sh
+++ nss/seed.sh
@@ -3,7 +3,7 @@ DESC="A set of libraries designed to support cross-platform development of secur
 VERSION="3.88.1"
 SOURCE="https://archive.mozilla.org/pub/security/nss/releases/NSS_3_88_1_RTM/src/nss-${VERSION}.tar.gz"
 CHECKSUM="7ca8ff8ed672d82290c3122f38708579"
-DEPS="nspr zlib p11-kit sqlite"
+DEPS="nspr zlib sqlite"
 FLAGS=""
 
 _setup()
