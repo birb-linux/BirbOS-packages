f73df39665f4cf16a287a0f7343ec936dd551514
Toasterbirb <git@toasterbirb.com>
2023-04-23 05:14:37 +0300

    Package iproute2

---
+++ iproute2/seed.sh
@@ -0,0 +1,31 @@
+NAME="iproute2"
+DESC="IPV4-based networking utilities"
+VERSION="6.2.0"
+SOURCE="https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-${VERSION}.tar.xz"
+CHECKSUM="85861856a856f6d358c6d9a4fc9088c9"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Disable arpd docs
+	sed -i /ARPD/d Makefile
+	rm -fv man/man8/arpd.8
+
+	make -j$(nproc) NETNS_RUN_DIR=/run/netns
+}
+
+_install()
+{
+	make SBINDIR=$FAKEROOT/$NAME/usr/sbin install
+
+	# Install the documentation
+	mkdir -pv             $FAKEROOT/$NAME/usr/share/doc/iproute2-${VERSION}
+	cp -v COPYING README* $FAKEROOT/$NAME/usr/share/doc/iproute2-${VERSION}
+}
