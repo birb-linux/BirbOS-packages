e236732656c0f81570d1ac3281d68b8b64f6290a
Toasterbirb <git@toasterbirb.com>
2023-04-23 13:11:09 +0300

    Package patch

---
+++ patch/seed.sh
@@ -0,0 +1,30 @@
+NAME="patch"
+DESC="Program for modifying or creating files by applying a 'patch' file typically created by the diff program"
+VERSION="2.7.6"
+SOURCE="https://ftp.gnu.org/gnu/patch/patch-${VERSION}.tar.xz"
+CHECKSUM="78ad9937e4caadcba1526ef1853730d5"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j$(nproc) check
+}
