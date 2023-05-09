e06d2cf4aca9ab704a9a2aee1f29f5784979e2b1
Toasterbirb <git@toasterbirb.com>
2023-05-10 01:20:33 +0300

    Package git

---
+++ git/seed.sh
@@ -0,0 +1,27 @@
+NAME="git"
+DESC="Distributed version control system"
+VERSION="2.39.2"
+SOURCE="https://www.kernel.org/pub/software/scm/git/git-${VERSION}.tar.xz"
+CHECKSUM="32d34dc65ae0955cc68c7152b5ca8b13"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr \
+            --with-gitconfig=/etc/gitconfig \
+            --with-python=python3
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make perllibdir=/usr/lib/perl5/5.36/site_perl install
+}
