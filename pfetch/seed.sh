b839e3e7282ec90926b4371b9efb4d91764fda6a
Toasterbirb <git@toasterbirb.com>
2023-05-10 01:39:04 +0300

    Package pfetch

---
+++ pfetch/seed.sh
@@ -0,0 +1,24 @@
+NAME="pfetch"
+DESC="A pretty system information tool written in POSIX sh"
+VERSION="0.6.0"
+SOURCE="https://github.com/dylanaraps/pfetch/archive/refs/tags/${VERSION}.tar.gz"
+CHECKSUM="934e54f1bad59bc53cd97bd799ed830c"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	printf ""
+}
+
+_install()
+{
+	# make prefix=$FAKEROOT/$NAME/usr install
+	cp -fv pfetch $FAKEROOT/$NAME/usr/bin/
+}
