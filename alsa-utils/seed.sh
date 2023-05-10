d1600472dd4f1034dc6179a2c0273c7209a39cb8
Toasterbirb <git@toasterbirb.com>
2023-05-10 18:31:19 +0300

    Package alsa-utils

---
+++ alsa-utils/seed.sh
@@ -0,0 +1,29 @@
+NAME="alsa-utils"
+DESC="Various utilities which are useful for controlling your sound card"
+VERSION="1.2.8"
+SOURCE="https://www.alsa-project.org/files/pub/utils/alsa-utils-${VERSION}.tar.bz2"
+CHECKSUM="51175f5cd413daf7009a80684c65113d"
+DEPS="alsa-lib ncurses"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --disable-alsaconf \
+			--prefix=$FAKEROOT/$NAME/usr \
+            --disable-bat   \
+            --disable-xmlto \
+            --with-curses=ncursesw
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
