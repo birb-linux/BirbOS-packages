39230d631e3c507d3e0d8b00626e2cd58c57ffb6
Toasterbirb <git@toasterbirb.com>
2023-05-10 08:13:37 +0300

    Package nasm

---
+++ nasm/seed.sh
@@ -0,0 +1,25 @@
+NAME="nasm"
+DESC="Netwide Assembler"
+VERSION="2.16.01"
+SOURCE="https://www.nasm.us/pub/nasm/releasebuilds/${VERSION}/nasm-${VERSION}.tar.xz"
+CHECKSUM="d755ba0d16f94616c2907f8cab7c748b"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
