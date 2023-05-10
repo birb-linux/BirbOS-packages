0adc2cbb8fb9b03f89dec31e04b6269e2f2f58ba
Toasterbirb <git@toasterbirb.com>
2023-05-10 06:42:23 +0300

    Package wayland-protocols

---
+++ wayland-protocols/seed.sh
@@ -0,0 +1,32 @@
+NAME="wayland-protocols"
+DESC="Additional Wayland protocols on top of Wayland core"
+VERSION="1.31"
+SOURCE="https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/${VERSION}/downloads/wayland-protocols-${VERSION}.tar.xz"
+CHECKSUM="1584de13eb30a4d1f2cd06c08ee24354"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson --prefix=$FAKEROOT/$NAME/usr --buildtype=release
+	ninja
+}
+
+_install()
+{
+	ninja install
+}
+
+_test()
+{
+	ninja test
+}
