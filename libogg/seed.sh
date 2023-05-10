b82a06ccce4904795fc42bced5c63b87019f6c77
Toasterbirb <git@toasterbirb.com>
2023-05-10 08:31:39 +0300

    Package libogg

---
+++ libogg/seed.sh
@@ -0,0 +1,32 @@
+NAME="libogg"
+DESC="Library for encoding and decoding Ogg files"
+VERSION="1.3.5"
+SOURCE="https://downloads.xiph.org/releases/ogg/libogg-${VERSION}.tar.xz"
+CHECKSUM="3178c98341559657a15b185bf5d700a5"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr    \
+            --disable-static \
+            --docdir=$FAKEROOT/$NAME/usr/share/doc/libogg-${VERSION}
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make check
+}
