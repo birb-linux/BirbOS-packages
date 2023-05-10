7d2f1547ff11a95aa5e99b4e3d6aca3bddbb9684
Toasterbirb <git@toasterbirb.com>
2023-05-10 08:54:07 +0300

    Package nmap

---
+++ nmap/seed.sh
@@ -0,0 +1,25 @@
+NAME="nmap"
+DESC="Utility for network exploration and security auditing"
+VERSION="7.93"
+SOURCE="https://nmap.org/dist/nmap-${VERSION}.tar.bz2"
+CHECKSUM="9027eac4b8ca57574012cb061ba9ce4d"
+DEPS="libpcap lua pcre liblinear"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
