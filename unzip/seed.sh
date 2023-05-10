58fdf3a356941136752a008170be0217c865cad1
Toasterbirb <git@toasterbirb.com>
2023-05-10 06:21:35 +0300

    Package unzip

---
+++ unzip/seed.sh
@@ -0,0 +1,27 @@
+NAME="unzip"
+DESC="ZIP extraction utilities"
+VERSION="60"
+SOURCE="https://downloads.sourceforge.net/infozip/unzip${VERSION}.tar.gz"
+CHECKSUM="62b490407489521db863b523a7f86375"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Apply some upstream fixes etc.
+	patch -Np1 -i $PKG_PATH/unzip-6.0-consolidated_fixes-1.patch
+
+	make -f unix/Makefile generic
+}
+
+_install()
+{
+	make prefix=$FAKEROOT/$NAME/usr MANDIR=$FAKEROOT/$NAME/usr/share/man/man1 \
+		 -f unix/Makefile install
+}
