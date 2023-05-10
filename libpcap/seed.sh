113cffde39b87d74ec526c214093afdfddf64d71
Toasterbirb <git@toasterbirb.com>
2023-05-10 08:49:19 +0300

    Package libpcap

---
+++ libpcap/seed.sh
@@ -0,0 +1,28 @@
+NAME="libpcap"
+DESC="Functions for user-level packet capture"
+VERSION="1.10.3"
+SOURCE="https://www.tcpdump.org/release/libpcap-${VERSION}.tar.gz"
+CHECKSUM="c42fde2eb75a5075f19116f8c9768573"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	# Skip installing the static libraries
+	sed -i '/INSTALL_DATA.*libpcap.a\|RANLIB.*libpcap.a/ s/^/#/' Makefile
+
+	make install
+}
