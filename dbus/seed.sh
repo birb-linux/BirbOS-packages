4a6d7c07d8e603de75dc960e633082ea89fc35f2
Toasterbirb <git@toasterbirb.com>
2023-05-10 19:23:32 +0300

    Package dbus

---
+++ dbus/seed.sh
@@ -0,0 +1,43 @@
+NAME="dbus"
+DESC="Message bus system"
+VERSION="1.14.6"
+SOURCE="https://dbus.freedesktop.org/releases/dbus/dbus-${VERSION}.tar.xz"
+CHECKSUM="e809d7aa905ad82adeaf1d59db5445b1"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr     \
+            --sysconfdir=/etc                    \
+            --localstatedir=/var                 \
+            --runstatedir=/run                   \
+            --enable-user-session                \
+            --disable-doxygen-docs               \
+            --disable-xml-docs                   \
+            --disable-static                     \
+            --with-systemduserunitdir=no         \
+            --with-systemdsystemunitdir=no       \
+            --docdir=$FAKEROOT/$NAME/usr/share/doc/dbus-${VERSION} \
+            --with-system-socket=/run/dbus/system_bus_socket
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+
+	# Make it possible to compile stuff depending on dbus
+	# whilst in chroot
+	dbus-uuidgen --ensure
+
+	# Create a symlink needed for elogind support
+	ln -sfv /var/lib/dbus/machine-id /etc
+}
