f8c455257cfd5f2dc916ba1703827e3f20f0165b
Toasterbirb <git@toasterbirb.com>
2023-05-10 19:05:32 +0300

    Package libtheora

---
+++ libtheora/seed.sh
@@ -0,0 +1,33 @@
+NAME="libtheora"
+DESC="Reference implementation of the Theora video compression format"
+VERSION="1.1.1"
+SOURCE="https://downloads.xiph.org/releases/theora/libtheora-${VERSION}.tar.xz"
+CHECKSUM="9eeabf1ad65b7f41533854a59f7a716d"
+DEPS="libogg libvorbis"
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Fix a bug with libpng 1.6
+	sed -i 's/png_\(sizeof\)/\1/g' examples/png2theora.c &&
+
+	./configure --prefix=$FAKEROOT/$NAME/usr --disable-static
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j${MAKEOPTS} check
+}
