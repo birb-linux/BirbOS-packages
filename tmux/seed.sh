585f6f372738ddbcc9b2b7b705bfe6184379dadb
Toasterbirb <git@toasterbirb.com>
2023-05-10 06:52:31 +0300

    Fix a misconfigured checksum

--- tmux/seed.sh
+++ tmux/seed.sh
@@ -2,7 +2,7 @@ NAME="tmux"
 DESC="terminal multiplexer"
 VERSION="3.3a"
 SOURCE="https://github.com/tmux/tmux/archive/refs/tags/${VERSION}.tar.gz"
-CHECKSUM="8eee5883b9f6550fa4efaad0c9ec38f3"
+CHECKSUM="38b341836ed0bfe98d55d32f045d64dd"
 DEPS=""
 FLAGS=""
 
