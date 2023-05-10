0c090a31e48c4f44f812f59990fdc1cd35102ed2
Toasterbirb <git@toasterbirb.com>
2023-05-10 06:55:08 +0300

    Some more fiddling with the source and checksum

--- tmux/seed.sh
+++ tmux/seed.sh
@@ -1,8 +1,8 @@
 NAME="tmux"
 DESC="terminal multiplexer"
 VERSION="3.3a"
-SOURCE="https://github.com/tmux/tmux/archive/refs/tags/${VERSION}.tar.gz"
-CHECKSUM="38b341836ed0bfe98d55d32f045d64dd"
+SOURCE="https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz"
+CHECKSUM="8eee5883b9f6550fa4efaad0c9ec38f3"
 DEPS=""
 FLAGS=""
 
