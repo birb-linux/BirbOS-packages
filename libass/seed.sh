4e543f0f49d49ad15f3f1f04bef08ea1587ce547
Toasterbirb <git@toasterbirb.com>
2023-05-10 08:19:57 +0300

    Package libass

---
+++ libass/seed.sh
@@ -0,0 +1,25 @@
+NAME="libass"
+DESC="Portable subtitle renderer for the ASS/SSA subtitle format"
+VERSION="0.17.0"
+SOURCE="https://github.com/libass/libass/releases/download/${VERSION}/libass-${VERSION}.tar.xz"
+CHECKSUM="25f7435779aa28eb7dbd3f76f4d17d15"
+DEPS="freetype fribidi nasm fontconfig"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr --disable-static
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+}
