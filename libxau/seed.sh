d3c4509dd779aedc6bd7db4ce7888bf6f881290e
Toasterbirb <git@toasterbirb.com>
2023-05-11 02:45:14 +0300

    Package libxau

---
+++ libxau/seed.sh
@@ -0,0 +1,30 @@
+NAME="libxau"
+DESC="Library that implements the X11 Authorization Protocol"
+VERSION="1.0.11"
+SOURCE="https://www.x.org/pub/individual/lib/libXau-${VERSION}.tar.xz"
+CHECKSUM="7f14ba9c84a81a2b9dd023706febab38"
+DEPS="xorgproto"
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j${MAKEOPTS} check
+}
