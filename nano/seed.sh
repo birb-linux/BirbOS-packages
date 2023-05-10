28df07cd1a4291938f017a9fd5fb65b5a2d73d9c
Toasterbirb <git@toasterbirb.com>
2023-05-10 05:33:16 +0300

    Package nano

---
+++ nano/seed.sh
@@ -0,0 +1,30 @@
+NAME="nano"
+DESC="Small, simple text editor"
+VERSION="7.2"
+MAJOR_VERSION="$(echo $VERSION | cut -d'.' -f1)"
+SOURCE="https://www.nano-editor.org/dist/v${MAJOR_VERSION}/nano-${VERSION}.tar.xz"
+CHECKSUM="13742c686c6ddb0b7b294634f0c13cec"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr \
+            --sysconfdir=/etc \
+            --enable-utf8     \
+            --docdir=$FAKEROOT/$NAME/usr/share/doc/nano-${VERSION}
+
+	make -j$(nproc)
+}
+
+_install()
+{
+	make install
+	install -v -m644 doc/{nano.html,sample.nanorc} $FAKEROOT/$NAME/usr/share/doc/nano-${VERSION}
+}
