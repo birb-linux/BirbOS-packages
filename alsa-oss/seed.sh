702f726d594cb8131ffa1b6dc92c6e3c235182ca
Toasterbirb <git@toasterbirb.com>
2023-05-10 18:37:38 +0300

    Package alsa-oss

---
+++ alsa-oss/seed.sh
@@ -0,0 +1,25 @@
+NAME="alsa-oss"
+DESC="alsa-oss compatibility library for programs that use alsa-oss sound interface"
+VERSION="1.1.8"
+SOURCE="https://www.alsa-project.org/files/pub/oss-lib/alsa-oss-${VERSION}.tar.bz2"
+CHECKSUM="9ec4bb783fdce19032aace086d65d874"
+DEPS="alsa-lib"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --disable-static --prefix=$FAKEROOT/$NAME/usr
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
