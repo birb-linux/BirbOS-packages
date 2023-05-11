4f46947aefd839595e7d8db7c2f1ab1b58590132
Toasterbirb <git@toasterbirb.com>
2023-05-11 18:10:10 +0300

    Move the sudo libs to fakeroot

--- sudo/seed.sh
+++ sudo/seed.sh
@@ -29,7 +29,10 @@ _build()
 _install()
 {
 	make install
-	mkdir -p $FAKEROOT/$NAME/usr/lib/sudo
+
+	# Move the sudo lib directory back into the fakeroot, because
+	# sudo doesn't seem to be fully respecting the --prefix variable
+	mv -vf /usr/lib/sudo $FAKEROOT/$NAME/usr/lib/sudo
 	ln -sfv libsudo_util.so.0.0.0 $FAKEROOT/$NAME/usr/lib/sudo/libsudo_util.so.0
 
 	# Allow the members of the wheel group to run commands as the root user
