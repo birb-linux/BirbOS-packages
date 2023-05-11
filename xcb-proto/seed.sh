9848306b6cde2b037aa572637cc6808ccf3c5614
Toasterbirb <git@toasterbirb.com>
2023-05-11 03:18:26 +0300

    Package xcb-proto

---
+++ xcb-proto/seed.sh
@@ -0,0 +1,28 @@
+NAME="xcb-proto"
+DESC="XML-XCB protocol descriptions used by libxcb"
+VERSION="1.15.2"
+SOURCE="https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-${VERSION}.tar.xz"
+CHECKSUM="d2251b01927c2fbe92dc540f9536b430"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	PYTHON=python3 ./configure $XORG_CONFIG
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	make -j${MAKEOPTS} check
+}
