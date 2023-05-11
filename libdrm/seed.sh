b2e18eb159a8296fb99b4e8b75b0dc987ecc5768
Toasterbirb <git@toasterbirb.com>
2023-05-11 03:29:37 +0300

    Package libdrm

---
+++ libdrm/seed.sh
@@ -0,0 +1,30 @@
+NAME="libdrm"
+DESC="Userspace library for accessing the direct rendering manager (DRM)"
+VERSION="2.4.115"
+SOURCE="https://dri.freedesktop.org/libdrm/libdrm-${VERSION}.tar.xz"
+CHECKSUM="5403981a20c964f4c893ff91393652bd"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson setup  --prefix=$XORG_PREFIX \
+				 --buildtype=release   \
+				 -Dudev=true           \
+				 -Dvalgrind=disabled
+	ninja
+}
+
+_install()
+{
+	ninja install
+}
