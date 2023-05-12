1618b0881760b7b88e5b73764b237a1f727ffebf
Toasterbirb <git@toasterbirb.com>
2023-05-12 04:11:15 +0300

    Package polkit

---
+++ polkit/seed.sh
@@ -0,0 +1,54 @@
+NAME="polkit"
+DESC="Toolkit for defining and handling authorizations"
+VERSION="122"
+SOURCE="https://gitlab.freedesktop.org/polkit/polkit/-/archive/${VERSION}/polkit-${VERSION}.tar.gz"
+CHECKSUM="bbe3e745fc5bc1a41f1b5044f09a0f26"
+DEPS="glib duktape linux-pam"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Polkit needs its own user
+	groupadd -fg 27 polkitd
+	useradd -c "PolicyKit Daemon Owner" -d /etc/polkit-1 -u 27 \
+			-g polkitd -s /bin/false polkitd
+
+	mkdir build
+	cd    build
+
+	meson --prefix=$FAKEROOT/$NAME/usr  \
+		  --buildtype=release           \
+		  -Dman=false                   \
+		  -Dsession_tracking=libelogind \
+		  -Dsystemdsystemunitdir=/tmp   \
+		  -Dtests=false                 \
+		  -Djs_engine=duktape           \
+		  ..
+
+	ninja
+
+	# Configure linux-pam
+	mkdir -p $FAKEROOT/$NAME/etc/pam.d
+	cat > $FAKEROOT/$NAME/etc/pam.d/polkit-1 << "EOF"
+# Begin /etc/pam.d/polkit-1
+
+auth     include        system-auth
+account  include        system-account
+password include        system-password
+session  include        system-session
+
+# End /etc/pam.d/polkit-1
+EOF
+}
+
+_install()
+{
+	ninja install
+	rm -v /tmp/*.service
+}
