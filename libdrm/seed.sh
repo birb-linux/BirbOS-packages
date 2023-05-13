628fb1f4510d89b78683a4c961cf03561069d36d
Toasterbirb <git@toasterbirb.com>
2023-05-13 20:17:28 +0300

    Add xorg libraries to libdrm dependency list

--- libdrm/seed.sh
+++ libdrm/seed.sh
@@ -3,7 +3,7 @@ DESC="Userspace library for accessing the direct rendering manager (DRM)"
 VERSION="2.4.115"
 SOURCE="https://dri.freedesktop.org/libdrm/libdrm-${VERSION}.tar.xz"
 CHECKSUM="5403981a20c964f4c893ff91393652bd"
-DEPS=""
+DEPS="xtrans libx11 libxext libfs libice libsm libxscrnsaver libxt libxmu libxpm libxaw libxfixes libxcomposite libxrender libxcursor libxdamage libfontenc libxfont2 libxft libxi libxinerama libxrandr libxres libxtst libxv libxvmc libxxf86dga libxxf86vm libdmx libpciaccess libxkbfile libxshmfence"
 FLAGS=""
 
 _setup()
