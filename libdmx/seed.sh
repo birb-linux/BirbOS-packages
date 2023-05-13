a69c5e7de3f7ff3c6731a9ad69c4cba33e9c7ebe
Toasterbirb <git@toasterbirb.com>
2023-05-13 18:04:44 +0300

    Package libdxm

---
+++ libdmx/seed.sh
@@ -0,0 +1,24 @@
+NAME="libdmx"
+DESC="X.Org dmx library"
+VERSION="1.1.4"
+SOURCE="https://www.x.org/archive//individual/lib/libdmx-${VERSION}.tar.bz2"
+CHECKSUM="d2f1f0ec68ac3932dd7f1d9aa0a7a11c"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libdmx-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
