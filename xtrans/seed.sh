8806fa98760324d29812ea012bea6393f7b54171
Toasterbirb <git@toasterbirb.com>
2023-05-13 03:58:10 +0300

    Package xtrans

---
+++ xtrans/seed.sh
@@ -0,0 +1,24 @@
+NAME="xtrans"
+DESC="X Transport Interface"
+VERSION="1.4.0"
+SOURCE="https://www.x.org/archive//individual/lib/xtrans-${VERSION}.tar.bz2"
+CHECKSUM="ce2fb8100c6647ee81451ebe388b17ad"
+DEPS="fontconfig libxcb elogind"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
