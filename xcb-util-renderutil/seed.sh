7ea2f2cf96c47efdba6fa0f24695f44fb0eec8fb
Toasterbirb <git@toasterbirb.com>
2023-05-13 19:59:11 +0300

    Package xcb-util-renderutil

---
+++ xcb-util-renderutil/seed.sh
@@ -0,0 +1,24 @@
+NAME="xcb-util-renderutil"
+DESC="Additional extensions to the XCB library"
+VERSION="0.3.10"
+SOURCE="https://xcb.freedesktop.org/dist/xcb-util-renderutil-${VERSION}.tar.xz"
+CHECKSUM="193b890e2a89a53c31e2ece3afcbd55f"
+DEPS="libxcb"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
