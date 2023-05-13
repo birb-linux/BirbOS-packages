a15648d3c4cedb8514caa2beef68e94ccd10cb0c
Toasterbirb <git@toasterbirb.com>
2023-05-13 17:41:11 +0300

    Add missing deps

--- libxinerama/seed.sh
+++ libxinerama/seed.sh
@@ -3,7 +3,7 @@ DESC="Xlib API for Xinerama extension to X11 Protocol"
 VERSION="1.1.5"
 SOURCE="https://www.x.org/archive//individual/lib/libXinerama-${VERSION}.tar.xz"
 CHECKSUM="228c877558c265d2f63c56a03f7d3f21"
-DEPS="fontconfig libxcb elogind libx11"
+DEPS="fontconfig libxcb elogind libx11 libxext"
 FLAGS=""
 
 _setup()
