49d4ce1f5e811389c9028062f7f949d2be9a8e4a
Toasterbirb <git@toasterbirb.com>
2023-05-13 16:55:41 +0300

    Package libxpm

---
+++ libxpm/seed.sh
@@ -0,0 +1,28 @@
+NAME="libxpm"
+DESC="X Pixmap (XPM) image file format library"
+VERSION="3.5.15"
+SOURCE="https://www.x.org/archive//individual/lib/libXpm-${VERSION}.tar.xz"
+CHECKSUM="b3c58c94e284fd6940d3615e660a0007"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXpm-${VERSION}
+}
+
+_build()
+{
+	sed -i '/TestAll.*TRUE/s|^|//|' test/TestAllFiles.h
+
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION \
+		--disable-open-zfile
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
