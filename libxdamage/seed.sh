210c7b325ecfe0fb0cee37aca917937341b2c064
Toasterbirb <git@toasterbirb.com>
2023-05-13 17:26:49 +0300

    Package libxdamage

---
+++ libxdamage/seed.sh
@@ -0,0 +1,24 @@
+NAME="libxdamage"
+DESC="X Damage Extension library"
+VERSION="1.1.6"
+SOURCE="https://www.x.org/archive//individual/lib/libXdamage-${VERSION}.tar.xz"
+CHECKSUM="ca55d29fa0a8b5c4a89f609a7952ebf8"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXdamage-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
