f897c9b225caa09d1c451dc4ccc4f54e8a66ecd3
Toasterbirb <git@toasterbirb.com>
2023-05-13 17:35:37 +0300

    Package libxft

---
+++ libxft/seed.sh
@@ -0,0 +1,24 @@
+NAME="libxft"
+DESC="X FreeType library"
+VERSION="2.3.7"
+SOURCE="https://www.x.org/archive//individual/lib/libXft-${VERSION}.tar.xz"
+CHECKSUM="69dc24ba444a0f6bd99423dbf8fd0260"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXft-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
