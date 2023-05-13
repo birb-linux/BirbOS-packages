26eaf4e3ca372ea0b5bfea1c9937e16e1e443b23
Toasterbirb <git@toasterbirb.com>
2023-05-13 16:43:51 +0300

    Package libxmu

---
+++ libxmu/seed.sh
@@ -0,0 +1,24 @@
+NAME="libxmu"
+DESC="Collection of miscellaneous utility functions that have been useful in building various applications and widgets"
+VERSION="1.1.4"
+SOURCE="https://www.x.org/archive//individual/lib/libXmu-${VERSION}.tar.xz"
+CHECKSUM="ed52d396115fbc4d05300762aab79685"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXmu-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
