2a6c10f19526500eff5bbc378e4d98cfe1387f3c
Toasterbirb <git@toasterbirb.com>
2023-05-13 17:40:43 +0300

    Package libxinerama

---
+++ libxinerama/seed.sh
@@ -0,0 +1,24 @@
+NAME="libxinerama"
+DESC="Xlib API for Xinerama extension to X11 Protocol"
+VERSION="1.1.5"
+SOURCE="https://www.x.org/archive//individual/lib/libXinerama-${VERSION}.tar.xz"
+CHECKSUM="228c877558c265d2f63c56a03f7d3f21"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXinerama-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
