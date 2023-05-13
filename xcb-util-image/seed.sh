576ae9652c71f7db9547a44bc730e12bc553a3c1
Toasterbirb <git@toasterbirb.com>
2023-05-13 19:52:38 +0300

    Package xcb-util-image

---
+++ xcb-util-image/seed.sh
@@ -0,0 +1,29 @@
+NAME="xcb-util-image"
+DESC="Additional extensions to the XCB library"
+VERSION="0.4.1"
+SOURCE="https://xcb.freedesktop.org/dist/xcb-util-image-${VERSION}.tar.xz"
+CHECKSUM="a67bfac2eff696170259ef1f5ce1b611"
+DEPS="xcb-util"
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
+
+_test()
+{
+	LD_LIBRARY_PATH=$XORG_PREFIX/lib make -j${MAKEOPTS} check
+}
