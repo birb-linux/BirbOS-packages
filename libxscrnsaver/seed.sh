486cf4d542e1d246d7cbb85aafc07724cca3d8bb
Toasterbirb <git@toasterbirb.com>
2023-05-13 16:20:28 +0300

    Package libxscrnsaver

---
+++ libxscrnsaver/seed.sh
@@ -0,0 +1,24 @@
+NAME="libxscrnsaver"
+DESC="Xlib-based X11 Screen Saver extension client library"
+VERSION="1.2.4"
+SOURCE="https://www.x.org/archive//individual/lib/libXScrnSaver-${VERSION}.tar.xz"
+CHECKSUM="e613751d38e13aa0d0fd8e0149cec057"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXScrnSaver-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
