dc31d3c4e218a521064c9a8a974f2456e87400a0
Toasterbirb <git@toasterbirb.com>
2023-05-13 17:49:58 +0300

    Add missing deps

--- libxtst/seed.sh
+++ libxtst/seed.sh
@@ -3,7 +3,7 @@ DESC="Xlib-based library for XTEST & RECORD extensions"
 VERSION="1.2.4"
 SOURCE="https://www.x.org/archive//individual/lib/libXtst-${VERSION}.tar.xz"
 CHECKSUM="02f128fbf809aa9c50d6e54c8e57cb2e"
-DEPS="fontconfig libxcb elogind libx11"
+DEPS="fontconfig libxcb elogind libx11 libxext libxi"
 FLAGS=""
 
 _setup()
