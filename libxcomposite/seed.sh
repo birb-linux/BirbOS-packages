214f67a7d976c7059eff81eb753bbaab2cbdfdae
Toasterbirb <git@toasterbirb.com>
2023-05-13 17:06:32 +0300

    Package libXcomposite

---
+++ libxcomposite/seed.sh
@@ -0,0 +1,24 @@
+NAME="libxcomposite"
+DESC="Xlib-based client library for the Composite extension to the X11 protocol"
+VERSION="0.4.6"
+SOURCE="https://www.x.org/archive//individual/lib/libXcomposite-${VERSION}.tar.xz"
+CHECKSUM="af0a5f0abb5b55f8411cd738cf0e5259"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXcomposite-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
