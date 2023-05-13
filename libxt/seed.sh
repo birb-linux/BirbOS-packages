f510495f8a147b257d2f61cd70cc8d88c8da5943
Toasterbirb <git@toasterbirb.com>
2023-05-13 16:40:04 +0300

    Package libxt

---
+++ libxt/seed.sh
@@ -0,0 +1,26 @@
+NAME="libxt"
+DESC="X Toolkit Intrinsics library"
+VERSION="1.2.1"
+SOURCE="https://www.x.org/archive//individual/lib/libXt-${VERSION}.tar.bz2"
+CHECKSUM="b122ff9a7ec70c94dbbfd814899fffa5"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXt-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION \
+		--with-appdefaultdir=/etc/X11/app-defaults
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
