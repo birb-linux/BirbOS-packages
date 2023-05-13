b70f8ba474abb9b1974895f8dfbe74ef94ab82b4
Toasterbirb <git@toasterbirb.com>
2023-05-13 04:12:47 +0300

    Add a required dependency

--- libx11/seed.sh
+++ libx11/seed.sh
@@ -3,7 +3,7 @@ DESC="Core X11 protocol client library"
 VERSION="1.8.4"
 SOURCE="https://www.x.org/archive//individual/lib/libX11-${VERSION}.tar.xz"
 CHECKSUM="e932752126240f0846b35eef6b1f2c3d"
-DEPS="fontconfig libxcb elogind"
+DEPS="fontconfig libxcb elogind xtrans"
 FLAGS=""
 
 _setup()
