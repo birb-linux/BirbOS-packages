03f3b00ec7c74640d2e3f65f26aa0a1cf85775ed
Toasterbirb <git@toasterbirb.com>
2023-05-13 20:03:34 +0300

    Package xcb-util-cursor

---
+++ xcb-util-cursor/seed.sh
@@ -0,0 +1,24 @@
+NAME="xcb-util-cursor"
+DESC="A module that implements the XCB cursor library (XCB replacement for libXcursor)"
+VERSION="0.1.4"
+SOURCE="https://xcb.freedesktop.org/dist/xcb-util-cursor-${VERSION}.tar.xz"
+CHECKSUM="0d244518ad54b886413fe782235d6210"
+DEPS="xcb-util-image xcb-util-renderutil"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
