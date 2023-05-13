2083b0d1f0c5759b89f87574562f9853e414aef5
Toasterbirb <git@toasterbirb.com>
2023-05-13 16:18:04 +0300

    Add a missing libice dependency

--- libsm/seed.sh
+++ libsm/seed.sh
@@ -3,7 +3,7 @@ DESC="X Session Management Library"
 VERSION="1.2.4"
 SOURCE="https://www.x.org/archive//individual/lib/libSM-${VERSION}.tar.xz"
 CHECKSUM="ffa434ed96ccae45533b3d653300730e"
-DEPS="fontconfig libxcb elogind libx11"
+DEPS="fontconfig libxcb elogind libx11 libice"
 FLAGS=""
 
 _setup()
