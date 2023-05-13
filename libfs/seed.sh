de7f70e299667486d363a7967f7eb503693e018e
Toasterbirb <git@toasterbirb.com>
2023-05-13 16:12:02 +0300

    Package libfs

---
+++ libfs/seed.sh
@@ -0,0 +1,24 @@
+NAME="libfs"
+DESC="filesystem API written in ANSI C"
+VERSION="1.0.9"
+SOURCE="https://www.x.org/archive//individual/lib/libFS-${VERSION}.tar.xz"
+CHECKSUM="742863a552ecd53cdb957b7b276213cc"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXext-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
