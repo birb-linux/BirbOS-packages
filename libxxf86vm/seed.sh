bf9386804222ac7d902941db4cae1c96fca27783
Toasterbirb <git@toasterbirb.com>
2023-05-13 18:01:31 +0300

    Package libxxf86vm

---
+++ libxxf86vm/seed.sh
@@ -0,0 +1,24 @@
+NAME="libXxf86vm"
+DESC="Xlib-based library for the XFree86-VidMode X extension"
+VERSION="1.1.5"
+SOURCE="https://www.x.org/archive//individual/lib/libXxf86vm-${VERSION}.tar.xz"
+CHECKSUM="5b913dac587f2de17a02e17f9a44a75f"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXxf86vm-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
