dc08734484f7565b243ebe2c72dd63001e032a0d
Toasterbirb <git@toasterbirb.com>
2023-05-13 17:55:15 +0300

    Package libxvmc

---
+++ libxvmc/seed.sh
@@ -0,0 +1,24 @@
+NAME="libxvmc"
+DESC="X.Org XvMC library"
+VERSION="1.0.13"
+SOURCE="https://www.x.org/archive//individual/lib/libXvMC-${VERSION}.tar.xz"
+CHECKSUM="11a358e7229fa28dc9801c1e64fe2e18"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXvMC-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
