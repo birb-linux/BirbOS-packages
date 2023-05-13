efc71c97091e2c484497f53d5ecf72e2cef216c7
Toasterbirb <git@toasterbirb.com>
2023-05-13 17:32:20 +0300

    Package libxfont2

---
+++ libxfont2/seed.sh
@@ -0,0 +1,26 @@
+NAME="libxfont2"
+DESC="X.Org Xfont library"
+VERSION="2.0.6"
+SOURCE="https://www.x.org/archive//individual/lib/libXfont2-${VERSION}.tar.xz"
+CHECKSUM="c179daa707f5f432f1bc13977e5bb329"
+DEPS="fontconfig libxcb elogind libx11"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd libXfont2-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG --docdir=$XORG_PREFIX/share/doc/$NAME-$VERSION \
+		--disable-devel-docs
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
