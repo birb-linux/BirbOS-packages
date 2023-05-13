a2898124603577f92937961493aa0f3e0ec13dee
Toasterbirb <git@toasterbirb.com>
2023-05-13 19:46:09 +0300

    Package libxcvt

---
+++ libxcvt/seed.sh
@@ -0,0 +1,27 @@
+NAME="libxcvt"
+DESC="A library providing a standalone version of the X server implementation of the VESA CVT standard timing modelines generator"
+VERSION="0.1.2"
+SOURCE="https://www.x.org/pub/individual/lib/libxcvt-${VERSION}.tar.xz"
+CHECKSUM="b553fdb6024c5a137ff925bf4c337724"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson --prefix=$XORG_PREFIX --buildtype=release ..
+	ninja
+}
+
+_install()
+{
+	ninja install
+}
