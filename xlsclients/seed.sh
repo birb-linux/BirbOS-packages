019a714c2ae02df2f651e08a95c96e24a4e0e961
Toasterbirb <git@toasterbirb.com>
2023-05-14 18:37:51 +0300

    Package xlsclients

---
+++ xlsclients/seed.sh
@@ -0,0 +1,24 @@
+NAME="xlsclients"
+DESC="List client applications running on a display"
+VERSION="1.1.5"
+SOURCE="https://www.x.org/archive//individual/app/xlsclients-${VERSION}.tar.xz"
+CHECKSUM="ab4b3c47e848ba8c3e47c021230ab23a"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
