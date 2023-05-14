31c454a8285032be3045588a8159c5b560eefff3
Toasterbirb <git@toasterbirb.com>
2023-05-14 18:31:43 +0300

    Package xkbutils

---
+++ xkbutils/seed.sh
@@ -0,0 +1,24 @@
+NAME="xkbutils"
+DESC="Collection of small XKB utilities"
+VERSION="1.0.5"
+SOURCE="https://www.x.org/archive//individual/app/xkbutils-${VERSION}.tar.xz"
+CHECKSUM="cf65ca1aaf4c28772ca7993cfd122563"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
