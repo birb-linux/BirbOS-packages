dc7f7a9338a6b677f1a946be94f7a566eedfc602
Toasterbirb <git@toasterbirb.com>
2023-05-14 18:45:31 +0300

    Package xprop

---
+++ xprop/seed.sh
@@ -0,0 +1,24 @@
+NAME="xprop"
+DESC="Property displayer for X"
+VERSION="1.2.6"
+SOURCE="https://www.x.org/archive//individual/app/xprop-${VERSION}.tar.xz"
+CHECKSUM="33c090d8632a300e63efbf36edd6a333"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
