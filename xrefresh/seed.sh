eb02f79ee0a5f0156e3ba686915eb4ec9dca80be
Toasterbirb <git@toasterbirb.com>
2023-05-14 18:55:51 +0300

    Package xrefresh

---
+++ xrefresh/seed.sh
@@ -0,0 +1,24 @@
+NAME="xrefresh"
+DESC="Refresh all or part of an X screen"
+VERSION="1.0.7"
+SOURCE="https://www.x.org/archive//individual/app/xrefresh-${VERSION}.tar.xz"
+CHECKSUM="33b04489e417d73c90295bd2a0781cbb"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
