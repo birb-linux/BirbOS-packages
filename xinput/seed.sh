2437af2c85825b5b0cd642eb89c0152df93486dd
Toasterbirb <git@toasterbirb.com>
2023-05-14 18:25:14 +0300

    Package xinput

---
+++ xinput/seed.sh
@@ -0,0 +1,24 @@
+NAME="xinput"
+DESC="Utility to configure and test X input devices"
+VERSION="1.6.3"
+SOURCE="https://www.x.org/archive//individual/app/xinput-${VERSION}.tar.bz2"
+CHECKSUM="ac6b7432726008b2f50eba82b0e2dbe4"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
