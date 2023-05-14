24deb27e375498e8212c72c15aeed3314b200d63
Toasterbirb <git@toasterbirb.com>
2023-05-14 18:18:12 +0300

    Package xdriinfo

---
+++ xdriinfo/seed.sh
@@ -0,0 +1,24 @@
+NAME="xdriinfo"
+DESC="Query configuration information of DRI drivers"
+VERSION="1.0.7"
+SOURCE="https://www.x.org/archive//individual/app/xdriinfo-${VERSION}.tar.xz"
+CHECKSUM="34aff1f93fa54d6a64cbe4fee079e077"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
