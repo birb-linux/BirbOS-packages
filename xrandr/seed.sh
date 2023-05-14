ed15ae9d85e2e6e97a4f9a40d1db77756b392753
Toasterbirb <git@toasterbirb.com>
2023-05-14 18:48:04 +0300

    Package xrandr

---
+++ xrandr/seed.sh
@@ -0,0 +1,27 @@
+NAME="xrandr"
+DESC="Primitive command line interface to RandR extension"
+VERSION="1.5.2"
+SOURCE="https://www.x.org/archive//individual/app/xrandr-${VERSION}.tar.xz"
+CHECKSUM="f822a8d5f233e609d27cc22d42a177cb"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+
+	# Replace an undocumented (and possibly broken) script
+	rm -fv $XORG_PREFIX/bin/xkeystone
+}
