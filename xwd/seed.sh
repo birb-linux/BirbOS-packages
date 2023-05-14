c766aee70ff8308819b9cdb4947e74f442e4a9a3
Toasterbirb <git@toasterbirb.com>
2023-05-14 19:07:01 +0300

    Package xwd

---
+++ xwd/seed.sh
@@ -0,0 +1,24 @@
+NAME="xwd"
+DESC="Dump an image of an X window"
+VERSION="1.0.8"
+SOURCE="https://www.x.org/archive//individual/app/xwd-${VERSION}.tar.bz2"
+CHECKSUM="f783a209f2e3fa13253cedb65eaf9cdb"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
