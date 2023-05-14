e61df95bcdac8dc3bd9980f6f3a222941cb0219b
Toasterbirb <git@toasterbirb.com>
2023-05-14 18:11:24 +0300

    Package xcmsdb

---
+++ xcmsdb/seed.sh
@@ -0,0 +1,24 @@
+NAME="xcmsdb"
+DESC="Device Color Characterization utility for X Color Management System"
+VERSION="1.0.6"
+SOURCE="https://www.x.org/archive//individual/app/xcmsdb-${VERSION}.tar.xz"
+CHECKSUM="82a90e2feaeab5c5e7610420930cc0f4"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
