36d344b4db55fc183c32101bac67a4689d2c6bba
Toasterbirb <git@toasterbirb.com>
2023-05-14 19:12:27 +0300

    Package xwud

---
+++ xwud/seed.sh
@@ -0,0 +1,24 @@
+NAME="xwud"
+DESC="Image displayer for X"
+VERSION="1.0.6"
+SOURCE="https://www.x.org/archive//individual/app/xwud-${VERSION}.tar.xz"
+CHECKSUM="5ff5dc120e8e927dc3c331c7fee33fc3"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
