e5a31fac4ca3864b90a58806c78befe61ea70452
Toasterbirb <git@toasterbirb.com>
2023-05-14 17:47:59 +0300

    Package x11perf

---
+++ x11perf/seed.sh
@@ -0,0 +1,24 @@
+NAME="x11perf"
+DESC="X11 server performance test program"
+VERSION="1.6.1"
+SOURCE="https://www.x.org/archive//individual/app/x11perf-${VERSION}.tar.bz2"
+CHECKSUM="e96b56756990c56c24d2d02c2964456b"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
