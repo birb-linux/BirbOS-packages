3134b370dd4adfea1c89cb3c6aaab871b8866493
Toasterbirb <git@toasterbirb.com>
2023-05-14 17:41:36 +0300

    Package setxkbmap

---
+++ setxkbmap/seed.sh
@@ -0,0 +1,24 @@
+NAME="setxkbmap"
+DESC="Set the keyboard using the X Keyboard Extension"
+VERSION="1.3.3"
+SOURCE="https://www.x.org/archive//individual/app/setxkbmap-${VERSION}.tar.xz"
+CHECKSUM="2f72c7170cdbadc8ef786b2f9cfd4a69"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
