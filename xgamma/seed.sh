038eb039222d18576ffdde1cbdeb2511c3bc670f
Toasterbirb <git@toasterbirb.com>
2023-05-14 18:21:45 +0300

    Package xgamma

---
+++ xgamma/seed.sh
@@ -0,0 +1,24 @@
+NAME="xgamma"
+DESC="Alter a monitor's gamma correction through the X server"
+VERSION="1.0.7"
+SOURCE="https://www.x.org/archive//individual/app/xgamma-${VERSION}.tar.xz"
+CHECKSUM="41afaa5a68cdd0de7e7ece4805a37f11"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
