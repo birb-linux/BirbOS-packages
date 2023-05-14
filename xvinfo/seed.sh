b43f55c65d3b43e0ca53f5172575728a4630d3cc
Toasterbirb <git@toasterbirb.com>
2023-05-14 19:04:44 +0300

    Package xvinfo

---
+++ xvinfo/seed.sh
@@ -0,0 +1,24 @@
+NAME="xvinfo"
+DESC="Print out X-Video extension adaptor information"
+VERSION="1.1.5"
+SOURCE="https://www.x.org/archive//individual/app/xvinfo-${VERSION}.tar.xz"
+CHECKSUM="d698862e9cad153c5fefca6eee964685"
+DEPS="libpng mesa xbitmaps xcb-util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
