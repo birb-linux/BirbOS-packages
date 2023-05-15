fb48be9d1f4f9d55745cd298265b7cb11fc72452
Toasterbirb <git@toasterbirb.com>
2023-05-15 20:16:29 +0300

    Package xclock

---
+++ xclock/seed.sh
@@ -0,0 +1,24 @@
+NAME="xclock"
+DESC="A simple clock application which is used in the default xinit configuration"
+VERSION="1.1.1"
+SOURCE="https://www.x.org/pub/individual/app/xclock-${VERSION}.tar.xz"
+CHECKSUM="1273e3f4c85f1801be11a5247c382d07"
+DEPS="xorg-libs"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
