f4b31f257a28301a5690615cda9745bc2c5afdfe
Toasterbirb <git@toasterbirb.com>
2023-05-16 20:43:42 +0300

    Package mononoki

---
+++ mononoki/seed.sh
@@ -0,0 +1,22 @@
+NAME="mononoki"
+DESC="A font for programming and code review"
+VERSION="1.6"
+SOURCE="https://github.com/madmalik/mononoki/releases/download/${VERSION}/mononoki.zip"
+CHECKSUM="5ad3d3829730bb202a7f26ba58fdea08"
+DEPS="unzip fontconfig"
+FLAGS="font"
+
+_setup()
+{
+	unzip $DISTFILES/$(basename $SOURCE)
+}
+
+_build()
+{
+	printf ""
+}
+
+_install()
+{
+	cp -v *.ttf $FAKEROOT/$NAME/usr/share/fonts/TTF/
+}
