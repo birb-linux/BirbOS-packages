5a037fce46e1c19ed29f1dfe1acff72ccd5d714a
Toasterbirb <git@toasterbirb.com>
2023-05-16 20:32:57 +0300

    Package dde

---
+++ dde/seed.sh
@@ -0,0 +1,37 @@
+NAME="dde"
+DESC="Dynamic Desktop Environment"
+VERSION="1.0.0"
+SOURCE="https://github.com/Toasterbirb/dde/releases/download/${VERSION}/dde-${VERSION}.tar.gz"
+CHECKSUM="8434490227e7d0614b3711b350b6a62f"
+DEPS="xorg-libs pkg-config ncurses fontconfig"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd dde
+}
+
+_build()
+{
+	SRC_ROOT=$(pwd)
+
+	# Build everything
+	for i in dmenu dwm dwmblocks slock st
+	do
+		cd $i
+		make -j${MAKEOPTS}
+		cd $SRC_ROOT
+	done
+}
+
+_install()
+{
+	# Install everything
+	for i in dmenu dwm dwmblocks slock st
+	do
+		cd $i
+		make DESTDIR=$FAKEROOT/$NAME/usr install
+		cd $SRC_ROOT
+	done
+}
