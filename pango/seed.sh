db96c2967a03e01ef7554311aa9f0e28ee7c52ca
Toasterbirb <git@toasterbirb.com>
2023-05-17 20:21:53 +0300

    Remove the gobject-introspection dependency

--- pango/seed.sh
+++ pango/seed.sh
@@ -4,8 +4,10 @@ VERSION="1.50.12"
 SHORT_VERSION="$(echo $VERSION | awk -F'.' '{print $1 "." $2}')"
 SOURCE="https://download.gnome.org/sources/pango/${SHORT_VERSION}/pango-${VERSION}.tar.xz"
 CHECKSUM="fd4b0b23915d6a0255317f811bea4215"
-DEPS="fontconfig freetype harfbuzz fribidi glib cairo gobject-introspection xorg-libs"
+DEPS="fontconfig freetype harfbuzz fribidi glib cairo xorg-libs"
 FLAGS=""
+
+# TODO: Add gobject-introspection dependency to this package is GNOME support is needed
 
 _setup()
 {
@@ -18,7 +20,7 @@ _build()
 	mkdir build
 	cd    build
 
-	meson --prefix=/usr --buildtype=release --wrap-mode=nofallback ..
+	meson --prefix=/usr --buildtype=release --wrap-mode=nofallback -Dintrospection=disabled ..
 	ninja
 }
 
