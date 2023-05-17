11df99970baa9ee39912ece0d846d57980745065
Toasterbirb <git@toasterbirb.com>
2023-05-17 03:09:30 +0300

    Add more xorg deps

--- dde/seed.sh
+++ dde/seed.sh
@@ -3,7 +3,7 @@ DESC="Dynamic Desktop Environment"
 VERSION="1.0.0"
 SOURCE="https://github.com/Toasterbirb/dde/releases/download/${VERSION}/dde-${VERSION}.tar.gz"
 CHECKSUM="8434490227e7d0614b3711b350b6a62f"
-DEPS="xorg-libs pkg-config ncurses fontconfig mononoki"
+DEPS="xorg-apps xorg-libs xorg-server pkg-config ncurses fontconfig mononoki xf86-input-libinput xf86-video-fbdev"
 FLAGS=""
 
 _setup()
