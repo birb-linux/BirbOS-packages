a8c4280bcbd7524dd92e23d5c449ce67abd01170
Toasterbirb <git@toasterbirb.com>
2023-05-18 01:37:56 +0300

    Package speex

---
+++ speex/seed.sh
@@ -0,0 +1,27 @@
+NAME="speex"
+DESC="Audio compression format designed specifically for speech"
+VERSION="1.2.1"
+SOURCE="https://downloads.xiph.org/releases/speex/speex-${VERSION}.tar.gz"
+CHECKSUM="fe7bf610883ff202092b92c72fe0fe3e"
+DEPS="libogg"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr    \
+            --disable-static \
+            --docdir=$FAKEROOT/$NAME/usr/share/doc/speex-${VERSION}
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
