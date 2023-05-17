c6f9d26805ffece201e8f6448f74d97f53482bb2
Toasterbirb <git@toasterbirb.com>
2023-05-17 20:36:15 +0300

    Add xmlto dependency

--- shared-mime-info/seed.sh
+++ shared-mime-info/seed.sh
@@ -3,7 +3,7 @@ DESC="MIME database"
 VERSION="2.2"
 SOURCE="https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/${VERSION}/shared-mime-info-${VERSION}.tar.gz"
 CHECKSUM="06cb9e92e4211dc53fd52b7bfd586c78"
-DEPS="glib libxml2"
+DEPS="glib libxml2 xmlto"
 FLAGS=""
 
 _setup()
