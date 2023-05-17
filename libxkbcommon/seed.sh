ce77a7e99ecbf1194d36454f7b7df31bb8e25b7d
Toasterbirb <git@toasterbirb.com>
2023-05-17 21:59:02 +0300

    Package libxkbcommon

---
+++ libxkbcommon/seed.sh
@@ -0,0 +1,34 @@
+NAME="libxkbcommon"
+DESC="Keymap compiler and support library which processes a reduced subset of keymaps as defined by the XKB specification"
+VERSION="1.5.0"
+SOURCE="https://xkbcommon.org/download/libxkbcommon-${VERSION}.tar.xz"
+CHECKSUM="40f0486b4eb491928ec6616c2ff85120"
+DEPS="xkeyboard-config libxcb"
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson --prefix=/usr       \
+		  --buildtype=release \
+		  -Denable-docs=false ..
+	ninja
+}
+
+_install()
+{
+	DESTDIR=$FAKEROOT/$NAME ninja install
+}
+
+_test()
+{
+	ninja test
+}
