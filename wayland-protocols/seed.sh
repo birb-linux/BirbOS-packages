3629d54307e53158085ccff20bfa25faccc41988
Toasterbirb <git@toasterbirb.com>
2023-05-18 01:13:02 +0300

    Compiling wayland-protolcs needs wayland-scanner

--- wayland-protocols/seed.sh
+++ wayland-protocols/seed.sh
@@ -3,7 +3,7 @@ DESC="Additional Wayland protocols on top of Wayland core"
 VERSION="1.31"
 SOURCE="https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/${VERSION}/downloads/wayland-protocols-${VERSION}.tar.xz"
 CHECKSUM="1584de13eb30a4d1f2cd06c08ee24354"
-DEPS=""
+DEPS="wayland"
 FLAGS="test"
 
 _setup()
