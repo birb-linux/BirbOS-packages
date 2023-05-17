ba2b824adc990e5f380d86b45f7e8832e5997e0e
Toasterbirb <git@toasterbirb.com>
2023-05-17 20:16:53 +0300

    Package pango

---
+++ pango/seed.sh
@@ -0,0 +1,28 @@
+NAME="pango"
+DESC="Library for laying out and rendering text, with an emphasis on internationalization"
+VERSION="1.50.12"
+SHORT_VERSION="$(echo $VERSION | awk -F'.' '{print $1 "." $2}')"
+SOURCE="https://download.gnome.org/sources/pango/${SHORT_VERSION}/pango-${VERSION}.tar.xz"
+CHECKSUM="fd4b0b23915d6a0255317f811bea4215"
+DEPS="fontconfig freetype harfbuzz fribidi glib cairo gobject-introspection xorg-libs"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson --prefix=/usr --buildtype=release --wrap-mode=nofallback ..
+	ninja
+}
+
+_install()
+{
+	DESTDIR=$FAKEROOT/$NAME ninja install
+}
