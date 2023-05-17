f8ebc9d1f1416bae2f07a60f8b45c08ca99baf80
Toasterbirb <git@toasterbirb.com>
2023-05-18 00:17:13 +0300

    Package fdk-aac

---
+++ fdk-aac/seed.sh
@@ -0,0 +1,24 @@
+NAME="fdk-aac"
+DESC="Fraunhofer FDK AAC library"
+VERSION="2.0.2"
+SOURCE="https://downloads.sourceforge.net/opencore-amr/fdk-aac-${VERSION}.tar.gz"
+CHECKSUM="b41222194b31f570b3132bd622a9aef6"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr --disable-static
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
