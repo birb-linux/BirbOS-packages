ad62f2a1646407c3d0b94000d815cfd025f326ac
Toasterbirb <git@toasterbirb.com>
2023-05-18 00:39:36 +0300

    Package intel-vaapi-driver

---
+++ intel-vaapi-driver/seed.sh
@@ -0,0 +1,24 @@
+NAME="intel-vaapi-driver"
+DESC="VA-API user mode driver for Intel GEN Graphics family"
+VERSION="2.4.1"
+SOURCE="https://github.com/intel/intel-vaapi-driver/releases/download/${VERSION}/intel-vaapi-driver-${VERSION}.tar.bz2"
+CHECKSUM="073fce0f409559109ad2dd0a6531055d"
+DEPS="libva"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure $XORG_CONFIG
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
