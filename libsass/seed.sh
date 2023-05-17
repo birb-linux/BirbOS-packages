238d4b93387766bd48afda365edd0338ac2c7045
Toasterbirb <git@toasterbirb.com>
2023-05-17 19:40:42 +0300

    Separate libsass into its own package

---
+++ libsass/seed.sh
@@ -0,0 +1,26 @@
+NAME="libsass"
+DESC="Library required for building SassC"
+VERSION="3.6.5"
+SOURCE="https://github.com/sass/libsass/archive/${VERSION}/libsass-${VERSION}.tar.gz"
+CHECKSUM="c49765b9b3824dcd4a7423225ca28bad"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	autoreconf -fi
+
+	./configure --prefix=$FAKEROOT/$NAME/usr --disable-static
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
