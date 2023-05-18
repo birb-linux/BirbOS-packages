6ca2555c1506f14d4fff478b0d72a1e83bb1dee9
Toasterbirb <git@toasterbirb.com>
2023-05-18 16:53:26 +0300

    Package libssh2

---
+++ libssh2/seed.sh
@@ -0,0 +1,27 @@
+NAME="libssh2"
+DESC="Client-side C library implementing the SSH2 protocol"
+VERSION="1.10.0"
+SOURCE="https://www.libssh2.org/download/libssh2-${VERSION}.tar.gz"
+CHECKSUM="f604ba083fad23bf715a9ecccc9f57f4"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Fix an incompatibility issue with recent OpenSSH versions
+	patch -Np1 -i $PKG_PATH/libssh2-1.10.0-upstream_fix-1.patch
+
+	./configure --prefix=$FAKEROOT/$NAME/usr --disable-static
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
