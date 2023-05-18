fb4fa82cb62002b7fd8747f56918ac925f10cc35
Toasterbirb <git@toasterbirb.com>
2023-05-18 20:16:16 +0300

    Package c-ares

---
+++ c-ares/seed.sh
@@ -0,0 +1,27 @@
+NAME="c-ares"
+DESC="C library for asynchronous DNS requests"
+VERSION="1.19.0"
+SOURCE="https://c-ares.haxx.se/download/c-ares-${VERSION}.tar.gz"
+CHECKSUM="2668790bc92603e292d0fbcb25ded39c"
+DEPS="cmake"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	cmake  -DCMAKE_INSTALL_PREFIX=$FAKEROOT/$NAME/usr ..
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
