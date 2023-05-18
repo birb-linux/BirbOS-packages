d2827b086f8325fe11c2c18d63becbd5b942e5ca
Toasterbirb <git@toasterbirb.com>
2023-05-18 20:19:46 +0300

    Package nodejs

---
+++ nodejs/seed.sh
@@ -0,0 +1,41 @@
+NAME="nodejs"
+DESC="JavaScript runtime built on Chrome's V8 JavaScript engine"
+VERSION="18.14.1"
+SOURCE="https://nodejs.org/dist/v${VERSION}/node-v${VERSION}.tar.xz"
+CHECKSUM="09aaaa219c453d6d5307641cc41b76e1"
+DEPS="which c-ares icu libuv nghttp2 zlib"
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd node-v${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=$FAKEROOT/$NAME/usr \
+            --shared-cares         \
+            --shared-libuv         \
+            --shared-openssl       \
+            --shared-nghttp2       \
+            --shared-zlib          \
+            --with-intl=system-icu
+
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+	ln -sf node $FAKEROOT/$NAME/usr/share/doc/node-${VERSION}
+}
+
+_test()
+{
+	# One test is known to fail
+
+	set +e
+	make test-only
+	set -e
+}
