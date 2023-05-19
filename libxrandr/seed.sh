626200b149d560a6200bc1c99f4f56df0f6bee8b
toasterbirb <git@toasterbirb.com>
2023-05-19 12:24:30 +0000

    Add 32bit support to libxrandr

--- libxrandr/seed.sh
+++ libxrandr/seed.sh
@@ -4,7 +4,7 @@ VERSION="1.5.3"
 SOURCE="https://www.x.org/archive//individual/lib/libXrandr-${VERSION}.tar.xz"
 CHECKSUM="850cbc7c921c9d5d0135734b114ff6ac"
 DEPS="fontconfig libxcb elogind libx11 libxext libxrender"
-FLAGS=""
+FLAGS="32bit"
 
 _setup()
 {
@@ -21,4 +21,23 @@ _build()
 _install()
 {
 	make install
+}
+
+_build32()
+{
+	make distclean
+
+	LDFLAGS="-L/usr/lib32" CC="gcc -m32" ./configure \
+		$XORG_CONFIG \
+		--host=i686-pc-linux-gnu \
+		--libdir=/usr/lib32
+
+	make -j${MAKEOPTS}
+}
+
+_install32()
+{
+	make DESTDIR=$PWD/DESTDIR install
+	cp -Rv DESTDIR/usr/lib32/* $FAKEROOT/$NAME/usr/lib32
+	rm -rf DESTDIR
 }
