dbaf5d835ed009f571e80bf09641059f3192ca54
toasterbirb <git@toasterbirb.com>
2023-05-19 12:26:03 +0000

    Add 32bit support to libxext

--- libxext/seed.sh
+++ libxext/seed.sh
@@ -4,7 +4,7 @@ VERSION="1.3.5"
 SOURCE="https://www.x.org/archive//individual/lib/libXext-${VERSION}.tar.xz"
 CHECKSUM="964942427fcc5a9fa0539661421857c8"
 DEPS="fontconfig libxcb elogind libx11"
-FLAGS=""
+FLAGS="32bit"
 
 _setup()
 {
@@ -21,4 +21,23 @@ _build()
 _install()
 {
 	make install
+}
+
+_build32()
+{
+	make distclean
+
+	LDFLAGS="-L/usr/lib32" CC="gcc -m32" ./configure \
+		$XORG_CONFIG \
+		--host=i686-pc-linux-gnu \
+		--libdir=/usr/lib32
+
+	make -j${MAKEOPTS}
+}
+
+_install32()
+{
+	make DESTDIR=$PWD/DESTDIR install
+	cp -Rv DESTDIR/usr/lib32/* $FAKEROOT/$NAME/usr/lib32
+	rm -rf DESTDIR
 }
