192f26771d3ba77ae14561421e9a31f681fe7dd1
toasterbirb <git@toasterbirb.com>
2023-05-19 07:56:43 +0000

    Package neovim

---
+++ neovim/seed.sh
@@ -0,0 +1,25 @@
+NAME="neovim"
+DESC="Vim-fork focused on extensibility and usability"
+VERSION="0.9.0"
+SOURCE="https://github.com/neovim/neovim/archive/refs/tags/v${VERSION}.tar.gz"
+CHECKSUM="86b762639f29ee92d37f8ec0f78b7074"
+DEPS="cmake unzip curl gettext"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$FAKEROOT/$NAME/usr" \
+		CMAKE_BUILD_TYPE=Release \
+		-j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
