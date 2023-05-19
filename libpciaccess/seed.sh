fea28503aab48e67ee83656c0cc1c31735863198
toasterbirb <git@toasterbirb.com>
2023-05-19 14:37:29 +0000

    Enable 32bit support for libpciaccess

--- libpciaccess/seed.sh
+++ libpciaccess/seed.sh
@@ -4,7 +4,7 @@ VERSION="0.17"
 SOURCE="https://www.x.org/archive//individual/lib/libpciaccess-${VERSION}.tar.xz"
 CHECKSUM="1466cf950c914ad2db1dbb76c9a724db"
 DEPS="fontconfig libxcb elogind libx11"
-FLAGS=""
+FLAGS="32bit"
 
 _setup()
 {
@@ -21,4 +21,23 @@ _build()
 _install()
 {
 	make install
+}
+
+_build32()
+{
+	make distclean
+
+	LDFLAGS="-L/usr/lib32" CC="gcc -m32" ./configure \
+		$XORG_CONFIG \
+		--host=i686-pc-linux-gnu \
+		--libdir=/usr/lib32
+
+	make -j${MAKEOPTS}
+}
+
+_install32()
+{
+	make DESTDIR=$PWD/DESTDIR install
+	cp -Rv DESTDIR/usr/lib32/* $FAKEROOT/$NAME/usr/lib32
+	rm -rf DESTDIR
 }
