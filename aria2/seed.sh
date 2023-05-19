cb2a43885bbb5eec8c331b2b9c3192190f4f3897
toasterbirb <git@toasterbirb.com>
2023-05-19 05:58:07 +0000

    Package aria2

--- aria2/seed.sh
+++ aria2/seed.sh
@@ -3,7 +3,7 @@ DESC="Lightweight multi-protocol & multi-source command-line download utility"
 VERSION="1.36.0"
 SOURCE="https://github.com/aria2/aria2/releases/download/release-${VERSION}/aria2-${VERSION}.tar.xz"
 CHECKSUM="f11ff410bbe00385b4a1b20d22aa2598"
-DEPS="autoconf openssl libssh2 zlib libxml2 c-ares pkg-config gmp"
+DEPS="autoconf openssl libssh2 zlib libxml2 c-ares pkg-config gmp nettle"
 FLAGS=""
 
 _setup()
@@ -14,7 +14,6 @@ _setup()
 
 _build()
 {
-	autoreconf -i
 	./configure --prefix=$FAKEROOT/$NAME/usr
 	make -j${MAKEOPTS}
 }
