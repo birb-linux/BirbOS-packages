52dd565c92a42070a3924a792894a57d9793fc60
Toasterbirb <git@toasterbirb.com>
2023-05-19 06:31:24 +0300

    Package json-glib

---
+++ json-glib/seed.sh
@@ -0,0 +1,28 @@
+NAME="json-glib"
+DESC="JSON GLib package is a library providing serialization and deserialization support for the JavaScript Object Notation (JSON)"
+VERSION="1.6.6"
+SHORT_VERSION="$(echo $VERSION | awk -F'.' '{print $1 "." $2}')"
+SOURCE="https://download.gnome.org/sources/json-glib/${SHORT_VERSION}/json-glib-${VERSION}.tar.xz"
+CHECKSUM="9c40fcd8cdbf484dd1704480afefae14"
+DEPS="glib"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson --prefix=/usr --buildtype=release ..
+	ninja
+}
+
+_install()
+{
+	DESTDIR=$FAKEROOT/$NAME ninja install
+}
