15ff9d47bc8baa88bf0ececa107e1b6c558c6f01
toasterbirb <git@toasterbirb.com>
2023-05-19 11:11:02 +0000

    Add 32bit support to xcb-proto

--- xcb-proto/seed.sh
+++ xcb-proto/seed.sh
@@ -4,7 +4,7 @@ VERSION="1.15.2"
 SOURCE="https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-${VERSION}.tar.xz"
 CHECKSUM="d2251b01927c2fbe92dc540f9536b430"
 DEPS=""
-FLAGS="test"
+FLAGS="32bit test"
 
 _setup()
 {
@@ -25,4 +25,23 @@ _install()
 _test()
 {
 	make -j${MAKEOPTS} check
+}
+
+_build32()
+{
+	make distclean
+
+	PYTHON=python3 LDFLAGS="-L/usr/lib32" CC="gcc -m32" ./configure \
+		$XORG_CONFIG \
+		--host=i686-pc-linux-gnu \
+		--libdir=/usr/lib32
+
+	make -j${MAKEOPTS}
+}
+
+_install32()
+{
+	make DESTDIR=$PWD/DESTDIR install
+	cp -Rv DESTDIR/usr/lib32/* $FAKEROOT/$NAME/usr/lib32
+	rm -rf DESTDIR
 }
