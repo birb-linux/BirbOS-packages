a3645cffba7bab8eae2c385e6f392dcad40c05a2
Toasterbirb <git@toasterbirb.com>
2023-05-19 05:46:42 +0300

    Revert "Revert "Package boost""
    This reverts commit a71e684bccead72ea3bfb49c5c3b0a6e4c15f279.

---
+++ boost/seed.sh
@@ -0,0 +1,29 @@
+NAME="boost"
+DESC="Set of free peer-reviewed portable C++ source libraries"
+VERSION="1.81.0"
+ALT_VERSION="$(echo $VERSION | sed 's/\./_/g')"
+SOURCE="https://boostorg.jfrog.io/artifactory/main/release/${VERSION}/source/boost_${ALT_VERSION}.tar.bz2"
+CHECKSUM="3276c0637d1be8687740c550237ef999"
+DEPS="which"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	# Avoid breaking packages that use the phoenix module when compiling with gcc
+	sed -i '/#include.*phoenix.*tuple.hpp.*/d' \
+		boost/phoenix/stl.hpp
+
+	./bootstrap.sh --prefix=$FAKEROOT/$NAME/usr --with-python=python3
+	./b2 stage -j$(nproc) threading=multi link=shared
+}
+
+_install()
+{
+	./b2 install threading=multi link=shared
+}
