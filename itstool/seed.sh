a7d702322dea2302d0df574562543e9a2bc83451
toasterbirb <git@toasterbirb.com>
2023-05-19 18:22:45 +0000

    Package itstool

---
+++ itstool/seed.sh
@@ -0,0 +1,24 @@
+NAME="itstool"
+DESC="Extracts messages from XML files and outputs PO template files"
+VERSION="2.0.7"
+SOURCE="https://files.itstool.org/itstool/itstool-${VERSION}.tar.bz2"
+CHECKSUM="267a3bdc72a2d8abb1b824f2ea32ee9b"
+DEPS="docbook-xml"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	PYTHON=/usr/bin/python3 ./configure --prefix=$FAKEROOT/$NAME/usr
+	make -j${MAKEOPTS}
+}
+
+_install()
+{
+	make install
+}
