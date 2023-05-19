e60124fef347d6add3dbc43b3da12d1ed9cb659d
toasterbirb <git@toasterbirb.com>
2023-05-19 19:32:17 +0000

    Add 32bit support to glib

--- glib/seed.sh
+++ glib/seed.sh
@@ -5,7 +5,7 @@ SHORT_VERSION="$(echo "${VERSION}" | awk -F'.' '{print $1 "." $2}')"
 SOURCE="https://download.gnome.org/sources/glib/${SHORT_VERSION}/glib-${VERSION}.tar.xz"
 CHECKSUM="7561501d9f63f3418ddb23d2903cc968"
 DEPS="libxslt pcre2"
-FLAGS=""
+FLAGS="32bit"
 
 _setup()
 {
@@ -37,4 +37,31 @@ _install()
 
 	mkdir -p $FAKEROOT/$NAME/usr/share/doc/glib-${VERSION}
 	#cp -r ../docs/reference/{gio,glib,gobject} $FAKEROOT/$NAME/usr/share/doc/glib-${VERSION}
+}
+
+_build32()
+{
+	cd ..
+	rm -r build
+	mkdir build
+	cd    build
+
+	# libxslt will cause some errors, but they are harmless
+	set +e
+
+	meson --prefix=$FAKEROOT/$NAME/usr \
+		  --buildtype=release \
+		  -Dman=false          \
+		  ..
+
+	ninja
+
+	set -e
+}
+
+_install32()
+{
+	DESTDIR=$PWD/DESTDIR ninja install
+	cp -Rv DESTDIR/usr/lib32/* $FAKEROOT/$NAME/usr/lib32
+	rm -rf DESTDIR
 }
