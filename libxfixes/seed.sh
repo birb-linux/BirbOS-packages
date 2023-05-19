7327268ab4ff860f7be76e88d80b49eda7675fd4
toasterbirb <git@toasterbirb.com>
2023-05-19 14:30:54 +0000

    Add 32bit support to libxfixes

--- libxfixes/seed.sh
+++ libxfixes/seed.sh
@@ -4,7 +4,7 @@ VERSION="6.0.0"
 SOURCE="https://www.x.org/archive//individual/lib/libXfixes-${VERSION}.tar.bz2"
 CHECKSUM="86f182f487f4f54684ef6b142096bb0f"
 DEPS="fontconfig libxcb elogind libx11"
-FLAGS=""
+FLAGS="32bit"
 
 _setup()
 {
@@ -21,4 +21,23 @@ _build()
 _install()
 {
 	make install
+}
+
+_build32()
+{
+	make distclean
+
+	LDFLAGS="-L/usr/lib32" CC="gcc -m32" ./configure \
+		$XORG_CONFIG \
+		--host=i686-pc-linux-gnu \
+		--libdir=/usr/lib32
+
+	make -j${MAKEOPTS}
+}
+
+_install32()
+{
+	make DESTDIR=$PWD/DESTDIR install
+	cp -Rv DESTDIR/usr/lib32/* $FAKEROOT/$NAME/usr/lib32
+	rm -rf DESTDIR
 }
