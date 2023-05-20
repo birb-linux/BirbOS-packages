81bfa1031a470090d2f5f91654b1119267f9d5d7
toasterbirb <git@toasterbirb.com>
2023-05-20 14:13:33 +0000

    Disable broken 32bit build

--- libxml2/seed.sh
+++ libxml2/seed.sh
@@ -4,7 +4,7 @@ VERSION="2.10.4"
 SOURCE="https://download.gnome.org/sources/libxml2/$(echo "$VERSION" | awk -F '.' '{print $1 "." $2}')/libxml2-${VERSION}.tar.xz"
 CHECKSUM="76808c467a58c31e2dbd511e71d5fd13"
 DEPS="icu"
-FLAGS="32bit"
+FLAGS=""
 
 _setup()
 {
@@ -30,26 +30,26 @@ _install()
 	make install
 }
 
-_build32()
-{
-	make distclean
-
-	unset LD_FLAGS
-	unset PKG_CONFIG_PATH
-	./configure --prefix=/usr \
-            --sysconfdir=/etc       \
-            --disable-static        \
-            --with-history          \
-            --with-icu              \
-            PYTHON=/usr/bin/python3 \
-            --docdir=$FAKEROOT/$NAME/usr/share/doc/libxml2-${VERSION}
-
-	make -j${MAKEOPTS}
-}
-
-_install32()
-{
-	make DESTDIR=$PWD/DESTDIR install
-	cp -Rv DESTDIR/usr/local/lib/* $FAKEROOT/$NAME/usr/lib32
-	rm -rf DESTDIR
-}
+#_build32()
+#{
+#	make distclean
+#
+#	LD_FLAGS=""
+#	PKG_CONFIG_PATH=""
+#	./configure --prefix=/usr \
+#            --sysconfdir=/etc       \
+#            --disable-static        \
+#            --with-history          \
+#            --with-icu              \
+#            PYTHON=/usr/bin/python3 \
+#            --docdir=$FAKEROOT/$NAME/usr/share/doc/libxml2-${VERSION}
+#
+#	make -j${MAKEOPTS}
+#}
+#
+#_install32()
+#{
+#	make DESTDIR=$PWD/DESTDIR install
+#	cp -Rv DESTDIR/usr/local/lib/* $FAKEROOT/$NAME/usr/lib32
+#	rm -rf DESTDIR
+#}
