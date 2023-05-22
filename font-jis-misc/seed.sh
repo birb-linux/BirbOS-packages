a28014ee86fd77545926515c4e86e7306440f322
toasterbirb <git@toasterbirb.com>
2023-05-22 23:26:09 +0300

    Package font-jis-misc

---
+++ font-jis-misc/seed.sh
@@ -0,0 +1,25 @@
+NAME="font-jis-misc"
+DESC="X.Org JIS (japanese) fonts"
+VERSION="1.0.4"
+SOURCE="https://gitlab.freedesktop.org/xorg/font/jis-misc/-/archive/font-jis-misc-${VERSION}/jis-misc-font-jis-misc-${VERSION}.tar.gz"
+CHECKSUM="06bfe204d605a7fa9a3908b4fe12bf49"
+DEPS="bdftopcf fontconfig font-util"
+FLAGS="font"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd jis-misc-font-jis-misc-${VERSION}
+}
+
+_build()
+{
+	./autogen.sh
+	./configure --prefix=/usr --with-fontrootdir=/usr/share/fonts/font-jis-misc
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME install
+}
