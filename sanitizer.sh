5276bbba806f84f6e9bdc40aeee8ce6c6f3c5a30
toasterbirb <git@toasterbirb.com>
2023-05-24 00:04:34 +0300

    Reverse the xinit dir change

--- sanitizer.sh
+++ sanitizer.sh
@@ -35,7 +35,6 @@ grep "ln.* /[A-Za-z]" $PKG_NAME/seed.sh && echo "Possibly misconfigured ln comma
 grep "install.* /[A-Za-z]" $PKG_NAME/seed.sh && echo "Possibly misconfigured install command ^"
 grep "sed.* /[A-Za-z]" $PKG_NAME/seed.sh && echo "Possibly misconfigured sed command ^"
 grep "echo.* /[A-Za-z]" $PKG_NAME/seed.sh && echo "Possibly misconfigured echo command ^"
-grep "\--with-xinitdir=/etc/X11/app-defaults" $PKG_NAME/seed.sh && echo "Possibly misconfigured xinidir path ^"
 grep "\--with-app-defaults=/etc/X11/app-defaults" $PKG_NAME/seed.sh && echo "Possibly misconfigured app-defaults path ^"
 
 
