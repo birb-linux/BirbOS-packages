9a540f5d38d9291585c4117ba200166e373d6bee
toasterbirb <git@toasterbirb.com>
2023-05-25 02:53:34 +0300

    Package glu

---
+++ glu/seed.sh
@@ -0,0 +1,30 @@
+NAME="glu"
+DESC="Mesa OpenGL Utility library"
+VERSION="9.0.2"
+SOURCE="ftp://ftp.freedesktop.org/pub/mesa/glu/glu-${VERSION}.tar.xz"
+CHECKSUM="2b0f13fa5b949bfb3a995927c6e35125"
+DEPS="mesa"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson --prefix=/usr -Dgl_provider=gl --buildtype=release ..
+	ninja
+}
+
+_install()
+{
+	DESTDIR=$FAKEROOT/$NAME ninja install
+
+	# Remove a static library
+	rm -vf $FAKEROOT/$NAME/usr/lib/libGLU.a
+}
