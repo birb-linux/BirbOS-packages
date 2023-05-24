a9cb38526a0cb54fc66b0d863842da9754776792
toasterbirb <git@toasterbirb.com>
2023-05-25 02:38:01 +0300

    Package poppler

---
+++ poppler/seed.sh
@@ -0,0 +1,36 @@
+NAME="poppler"
+DESC="PDF rendering library and command line tools used to manipulate PDF files"
+VERSION="23.02.0"
+SOURCE="https://poppler.freedesktop.org/poppler-${VERSION}.tar.xz"
+CHECKSUM="ce7eef12c40dcd78998a43422c2c71ef"
+DEPS="cmake fontconfig gobject-introspection boost cairo little-cms libjpeg-turbo libpng nss openjpeg"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	cmake  -DCMAKE_BUILD_TYPE=Release   \
+		   -DCMAKE_INSTALL_PREFIX=/usr  \
+		   -DTESTDATADIR=$PWD/testfiles \
+		   -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
+		   ..
+
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME install
+
+	# Install the documentation
+	install -v -m755 -d           $FAKEROOT/$NAME/usr/share/doc/poppler-${VERSION}
+	cp -vr ../glib/reference/html $FAKEROOT/$NAME/usr/share/doc/poppler-${VERSION}
+}
