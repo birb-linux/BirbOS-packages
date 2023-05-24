5fb4c9b0e78424e0e323ca76ffc13496ad437dfd
toasterbirb <git@toasterbirb.com>
2023-05-24 18:14:15 +0300

    Package mit-kerberos5

---
+++ mit-kerberos5/seed.sh
@@ -0,0 +1,41 @@
+NAME="mit-kerberos5"
+DESC="Free implementation of Kerberos 5, a network authentication protocol"
+VERSION="1.20.1"
+SHORT_VERSION="$(echo $VERSION | awk -F'.' '{print $1 "." $2}')"
+SOURCE="https://kerberos.org/dist/krb5/${SHORT_VERSION}/krb5-${VERSION}.tar.gz"
+CHECKSUM="73f5780e7b587ccd8b8cfc10c965a686"
+DEPS="ntp"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd krb5-${VERSION}
+}
+
+_build()
+{
+	cd src &&
+
+	sed -i -e '/eq 0/{N;s/12 //}'    plugins/kdb/db2/libdb2/test/run.test
+	sed -i '/t_kadm5.py/d'           lib/kadm5/Makefile.in
+
+	./configure --prefix=/usr            \
+				--sysconfdir=/etc        \
+				--localstatedir=/var/lib \
+				--runstatedir=/run       \
+				--with-system-et         \
+				--with-system-ss         \
+				--with-system-verto=no   \
+				--enable-dns-for-realm
+
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME install
+
+	install -v -dm755 $FAKEROOT/$NAME/usr/share/doc/krb5-${VERSION}
+	cp -vfr ../doc/*  $FAKEROOT/$NAME/usr/share/doc/krb5-${VERSION}
+}
