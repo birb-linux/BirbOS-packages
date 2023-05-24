83535468594dad3049608738d6044e647ba0cc17
toasterbirb <git@toasterbirb.com>
2023-05-24 21:21:00 +0300

    Package librsvg

---
+++ librsvg/seed.sh
@@ -0,0 +1,33 @@
+NAME="librsvg"
+DESC="Library and tools used to manipulate, convert and view Scalable Vector Graphic (SVG) images"
+VERSION="2.54.5"
+SHORT_VERSION="$(echo $VERSION | awk -F'.' '{print $1 "." $2}')"
+SOURCE="https://download.gnome.org/sources/librsvg/${SHORT_VERSION}/librsvg-${VERSION}.tar.xz"
+CHECKSUM="eb56ffb37a7db48491177eb9bd135cb5"
+DEPS="cairo gdk-pixbuf pango rustc gobject-introspection"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=/usr    \
+            --disable-static \
+            --docdir=/usr/share/doc/librsvg-${VERSION}
+
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME install
+}
+
+_post_install()
+{
+	gdk-pixbuf-query-loaders --update-cache
+}
