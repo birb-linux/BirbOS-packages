15eb52ece582bff083d6c97ab27a415864734702
toasterbirb <git@toasterbirb.com>
2023-05-25 04:45:18 +0300

    Switch opus dependency to opusfile

--- cmus/seed.sh
+++ cmus/seed.sh
@@ -3,7 +3,7 @@ DESC="Small, fast and powerful console music player for Unix-like operating syst
 VERSION="2.9.1"
 SOURCE="https://github.com/cmus/cmus/archive/v${VERSION}.tar.gz"
 CHECKSUM="914ea4f79827174f825e9e7ce3880c44"
-DEPS="pulseaudio faac flac libmad opus libvorbis"
+DEPS="pulseaudio faac flac libmad opusfile libvorbis"
 FLAGS=""
 NOTES="cmus has a lot of optional dependencies. If there's some specific audio format that you need, please installed the required library for that and reinstall cmus. The most common formats (mp3, opus etc.) should be covered by the dependencies"
 
