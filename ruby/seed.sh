75b785d147c99f08d8baeb03986fc02aaf0945d6
toasterbirb <git@toasterbirb.com>
2023-05-25 17:10:18 +0300

    Package ruby

---
+++ ruby/seed.sh
@@ -0,0 +1,29 @@
+NAME="ruby"
+DESC="Ruby development environment"
+VERSION="3.2.1"
+SHORT_VERSION="$(echo $VERSION | awk -F'.' '{print $1 "." $2}')"
+SOURCE="https://cache.ruby-lang.org/pub/ruby/${SHORT_VERSION}/ruby-${VERSION}.tar.xz"
+CHECKSUM="23617540ad13c57ed8dd9d4ed32192a5"
+DEPS="libyaml"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=/usr      \
+            --enable-shared    \
+            --without-valgrind \
+            --docdir=/usr/share/doc/ruby-${VERSION}
+
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME install
+}
