f1ad7e06621c6f20d11bf34ae6af3a3dcb5ad2d8
toasterbirb <git@toasterbirb.com>
2023-06-01 09:00:32 +0300

    Package libslirp

---
+++ libslirp/seed.sh
@@ -0,0 +1,28 @@
+NAME="libslirp"
+DESC="User-mode networking library used by virtual machines, containers or various tools"
+VERSION="4.7.0"
+SOURCE="https://gitlab.freedesktop.org/slirp/libslirp/-/archive/v${VERSION}/libslirp-v${VERSION}.tar.bz2"
+CHECKSUM="b815c4de99265559caf5ef635a213609"
+DEPS="glib"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-v${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson setup --prefix=/usr --buildtype=release ..
+
+	ninja
+}
+
+_install()
+{
+	DESTDIR=$FAKEROOT/$NAME ninja install
+}
