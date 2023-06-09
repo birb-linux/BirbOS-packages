5a3165968073a82081eb049791b56bdda2f656d6
Toasterbirb <git@toasterbirb.com>
2023-06-09 17:01:08 +0300

    Update gdk-pixbuf cache during post-install

--- gdk-pixbuf/seed.sh
+++ gdk-pixbuf/seed.sh
@@ -28,8 +28,11 @@ _build()
 _install()
 {
 	DESTDIR=$FAKEROOT/$NAME ninja install
+}
 
-	$FAKEROOT/$NAME/usr/bin/gdk-pixbuf-query-loaders --update-cache
+_post_install()
+{
+	gdk-pixbuf-query-loaders --update-cache
 }
 
 _test()
