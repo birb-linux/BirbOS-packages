39cd32f0bb144e778c3b7a7817774e1b28dde30e
Toasterbirb <git@toasterbirb.com>
2023-06-13 16:57:44 +0300

    Bump the dde version

--- dde/seed.sh
+++ dde/seed.sh
@@ -1,15 +1,15 @@
 NAME="dde"
 DESC="Dynamic Desktop Environment"
-VERSION="1.0.0"
-SOURCE="https://github.com/Toasterbirb/dde/releases/download/${VERSION}/dde-${VERSION}.tar.gz"
-CHECKSUM="8434490227e7d0614b3711b350b6a62f"
+VERSION="1.0.1"
+SOURCE="https://github.com/Toasterbirb/dde/archive/refs/tags/${VERSION}.tar.gz"
+CHECKSUM="fa77b80be45df396513cc8625ff327f9"
 DEPS="xorg-apps xorg-libs xorg-server pkg-config ncurses fontconfig mononoki xf86-input-libinput xf86-video-fbdev xinit"
 FLAGS=""
 
 _setup()
 {
 	tar -xf $DISTFILES/$(basename $SOURCE)
-	cd dde
+	cd dde-${VERSION}
 }
 
 _build()
