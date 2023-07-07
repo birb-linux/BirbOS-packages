6b7d0ed0efc56039fa75670017707e5511c278f8
Toasterbirb <git.53jvg@aleeas.com>
2023-07-07 21:20:13 +0300

    Revert "Make sure that make overwrites BirbOS installation leftover files"
    This reverts commit 1dc74fc2d968c5e717fc0cd5e837f15faa5a4100.

--- make/seed.sh
+++ make/seed.sh
@@ -22,11 +22,6 @@ _build()
 _install()
 {
 	make install
-
-	# Make sure that the make locale files get overwritten
-	# if there are any leftover from the BirbOS installation
-	# process
-	rm -v /usr/share/locale/*/LC_MESSAGES/make.mo
 }
 
 _test()
