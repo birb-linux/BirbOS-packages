a0843f272183ee4fb3b63b03d253bfd6dc9ee64b
Toasterbirb <git.53jvg@aleeas.com>
2023-07-28 20:01:15 +0300

    Package vala

---
+++ vala/seed.sh
@@ -0,0 +1,25 @@
+NAME="vala"
+DESC="Programming language for GNOME developers"
+VERSION="0.56.4"
+SHORT_VERSION="$(echo $VERSION | awk -F'.' '{print $1 "." $2}')"
+SOURCE="https://download.gnome.org/sources/vala/${SHORT_VERSION}/vala-${VERSION}.tar.xz"
+CHECKSUM="dbb3d5b4616211e68093676099d44d56"
+DEPS="glib graphviz"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=/usr
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME install
+}
