dac8ffad1d1535f72b80d5db324472a53a8dab3c
Toasterbirb <git.53jvg@aleeas.com>
2023-07-29 22:02:16 +0300

    Fix the description

--- libmcrypt/seed.sh
+++ libmcrypt/seed.sh
@@ -1,5 +1,5 @@
 NAME="libmcrypt"
-DESC="Replacement for the old unix crypt"
+DESC="Provides an uniform interface to access several encryption algorithms"
 VERSION="2.5.8"
 SOURCE="https://sourceforge.net/projects/mcrypt/files/Libmcrypt/${VERSION}/libmcrypt-${VERSION}.tar.gz"
 CHECKSUM="0821830d930a86a5c69110837c55b7da"
