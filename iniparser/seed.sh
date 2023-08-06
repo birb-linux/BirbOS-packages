f8e7c11d7236c90e720f84ad35f6313997711000
Toasterbirb <git@toasterbirb.com>
2023-08-07 02:15:08 +0300

    Package iniparser

---
+++ iniparser/seed.sh
@@ -0,0 +1,29 @@
+NAME="iniparser"
+DESC="Standalone ini parser library in ANSI C"
+VERSION="4.1"
+SOURCE="https://github.com/ndevilla/iniparser/archive/refs/tags/v${VERSION}.tar.gz"
+CHECKSUM="e43b722c71b399ab17c329c04dbdf1d7"
+DEPS=""
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	install -v -m755 libiniparser.so.1 $FAKEROOT/$NAME/usr/lib
+	ln -srv $FAKEROOT/$NAME/usr/lib/libiniparser.so.1 $FAKEROOT/$NAME/usr/lib/libiniparser.so.0
+}
+
+_test()
+{
+	make check
+}
