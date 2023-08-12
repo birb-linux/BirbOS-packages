cf10fba825ab4ef612a8cfc1c3a4f194829635aa
Toasterbirb <git@toasterbirb.com>
2023-08-12 18:49:49 +0300

    Get X11 working

--- mpv/seed.sh
+++ mpv/seed.sh
@@ -3,7 +3,7 @@ DESC="Command line video player"
 VERSION="0.36.0"
 SOURCE="https://github.com/mpv-player/mpv/archive/refs/tags/v${VERSION}.tar.gz"
 CHECKSUM="3ac8bb1fec1c09293a574e615446bb3b"
-DEPS="xorg-libs pulseaudio ffmpeg zlib libass lua libjpeg-turbo uchardet"
+DEPS="xorg-libs pulseaudio ffmpeg zlib libass lua libjpeg-turbo uchardet xpresent"
 FLAGS=""
 
 _setup()
@@ -14,7 +14,7 @@ _setup()
 
 _build()
 {
-	meson setup --prefix=/usr build
+	meson setup --prefix=/usr -Dx11=enabled build
 	meson compile -C build
 }
 
