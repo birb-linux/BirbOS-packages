1ef7c514588b01d1d17ceb359989b45f7e3b668f
Toasterbirb <git@toasterbirb.com>
2023-08-12 18:30:35 +0300

    Package mpv

---
+++ mpv/seed.sh
@@ -0,0 +1,24 @@
+NAME="mpv"
+DESC="Command line video player"
+VERSION="0.36.0"
+SOURCE="https://github.com/mpv-player/mpv/archive/refs/tags/v${VERSION}.tar.gz"
+CHECKSUM="3ac8bb1fec1c09293a574e615446bb3b"
+DEPS="xorg-libs pulseaudio ffmpeg zlib libass lua libjpeg-turbo uchardet"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	meson setup build
+	meson compile -C build
+}
+
+_install()
+{
+	prefix=/usr DESTDIR=$FAKEROOT/$NAME meson install -C build
+}
