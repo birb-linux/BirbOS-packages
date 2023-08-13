f7bec4ae8a92a9815a47c8880b944ba66dbeff5e
Toasterbirb <git@toasterbirb.com>
2023-08-13 05:02:15 +0300

    Rework the yt-dlp package

--- yt-dlp/seed.sh
+++ yt-dlp/seed.sh
@@ -3,8 +3,8 @@ DESC="A youtube-dl fork with additional features and fixes"
 VERSION="2023.07.06"
 SOURCE="https://github.com/yt-dlp/yt-dlp/releases/download/${VERSION}/yt-dlp.tar.gz"
 CHECKSUM="20ff3cad19bda18fa3bf9e8492222645"
-DEPS="ffmpeg mutagen"
-FLAGS="python"
+DEPS="ffmpeg"
+FLAGS=""
 
 _setup()
 {
@@ -14,10 +14,10 @@ _setup()
 
 _build()
 {
-	pip3 wheel -w $FAKEROOT/$NAME/$PYTHON_DIST --no-build-isolation --no-deps $PWD
+	printf ""
 }
 
 _install()
 {
-	pip3 install --no-index --no-user --find-links $FAKEROOT/$NAME/$PYTHON_DIST $NAME
+	make DESTDIR=$FAKEROOT/$NAME PREFIX=/usr install
 }
