56e34a1f832e0a7a43ce6e696f5989d6d59344ba
Toasterbirb <git@toasterbirb.com>
2023-08-16 22:08:37 +0300

    Bump birb-utils version + follow the new makefile format

--- birb-utils/seed.sh
+++ birb-utils/seed.sh
@@ -1,8 +1,8 @@
 NAME="birb-utils"
 DESC="Miscellaneous utility scripts and programs made for BirbOS"
-VERSION="0.6.1"
+VERSION="0.6.2"
 SOURCE="https://github.com/Toasterbirb/birb-utils/archive/refs/tags/${VERSION}.tar.gz"
-CHECKSUM="133e930d27e1afe9afd4972e2774c3b3"
+CHECKSUM="289b2d873dc1b252136bd1936a175a90"
 DEPS="file wget curl sudo gnupg"
 FLAGS=""
 
@@ -19,5 +19,5 @@ _build()
 
 _install()
 {
-	make DESTDIR=$FAKEROOT/$NAME/usr install
+	make DESTDIR=$FAKEROOT/$NAME PREFIX=/usr install
 }
