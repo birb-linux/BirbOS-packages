1afb61358f5940ccda119ae255f7f547d98c37b5
Toasterbirb <git@toasterbirb.com>
2023-08-17 23:21:23 +0300

    Package xfconf

---
+++ xfconf/seed.sh
@@ -0,0 +1,25 @@
+NAME="xfconf"
+DESC="Configuration storage system for Xfce"
+VERSION="4.18.0"
+SHORT_VERSION="$(echo $VERSION | awk -F'.' '{print $1 "." $2}')"
+SOURCE="https://archive.xfce.org/src/xfce/xfconf/${SHORT_VERSION}/xfconf-${VERSION}.tar.bz2"
+CHECKSUM="efb7381405bc634306ed0d0cdf5aaffc"
+DEPS="libxfce4util"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=/usr
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME install
+}
