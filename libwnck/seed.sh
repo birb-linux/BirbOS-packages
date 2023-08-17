5a229862fd90e11191d9db064a456c5445c56bae
Toasterbirb <git@toasterbirb.com>
2023-08-17 23:12:55 +0300

    Package libwnck

---
+++ libwnck/seed.sh
@@ -0,0 +1,28 @@
+NAME="libwnck"
+DESC="Window Navigator Construction Kit"
+VERSION="43.0"
+MAJOR_VERSION="$(echo $VERSION | cut -d'.' -f1)"
+SOURCE="https://download.gnome.org/sources/libwnck/${MAJOR_VERSION}/libwnck-${VERSION}.tar.xz"
+CHECKSUM="cd21ef743a1e9286554401c5b28d5ec6"
+DEPS="gtk+ startup-notification"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson --prefix=/usr --buildtype=release ..
+	ninja
+}
+
+_install()
+{
+	DESTDIR=$FAKEROOT/$NAME ninja install
+}
