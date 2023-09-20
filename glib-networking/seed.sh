a74cb625866ca44ed6faa4bfe31b3ee5871a5a2f
Toasterbirb <git@toasterbirb.com>
2023-09-20 21:10:45 +0300

    Fix glib-networking checksum

--- glib-networking/seed.sh
+++ glib-networking/seed.sh
@@ -3,7 +3,7 @@ DESC="Network related gio modules for GLib"
 VERSION="2.76.1"
 SHORT_VERSION="$(short_version $VERSION)"
 SOURCE="https://download.gnome.org/sources/glib-networking/${SHORT_VERSION}/glib-networking-${VERSION}.tar.xz"
-CHECKSUM="7fc6ca153af0833fdf5c65044a3f7c42"
+CHECKSUM="05b519bb1f009789e6b82af88ed1b59a"
 DEPS="glib gnutls make-ca"
 FLAGS="test"
 
