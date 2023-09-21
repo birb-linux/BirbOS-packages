7d4aa0c17b517d161e95e30d301070d87bff7788
Toasterbirb <git@toasterbirb.com>
2023-09-22 01:03:26 +0300

    Add some missing dependencies

--- desmume/seed.sh
+++ desmume/seed.sh
@@ -3,7 +3,7 @@ DESC="Nintendo DS emulator"
 VERSION="0_9_13"
 SOURCE="https://github.com/TASEmulators/desmume/archive/refs/tags/release_${VERSION}.tar.gz"
 CHECKSUM="d6c9bb4dd1dcaee9c0a5064663e66658"
-DEPS="alsa-lib gtk+ ninja meson sdl2"
+DEPS="glib libpcap zlib glu alsa-lib gtk+ ninja meson sdl2"
 FLAGS=""
 
 _setup()
@@ -21,5 +21,5 @@ _build()
 
 _install()
 {
-	DESTDIR=$FAKEROOT/$NAME ninja install
+	DESTDIR=$FAKEROOT/$NAME ninja -C build install
 }
