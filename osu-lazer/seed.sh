c0e8dc82774e4d4021538d18f540271ab0802bb9
Toasterbirb <git@toasterbirb.com>
2023-09-24 15:28:15 +0300

    Add missing fuse2 dependency for appimage support

--- osu-lazer/seed.sh
+++ osu-lazer/seed.sh
@@ -3,7 +3,7 @@ DESC="rhythm is just a *click* away!"
 VERSION="2023.914.0"
 SOURCE="https://github.com/ppy/osu/releases/download/${VERSION}/osu.AppImage"
 CHECKSUM="1f33c393496fdd068ce5e41e3ad6ae37"
-DEPS="pulseaudio alsa-plugins"
+DEPS="pulseaudio alsa-plugins fuse2"
 FLAGS="proprietary"
 
 _setup()
