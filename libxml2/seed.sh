fa422c5af371dd9855d18fc530dfda703a09eff5
Toasterbirb <git@toasterbirb.com>
2023-10-04 23:00:55 +0300

    Switch libxml2 to using destdir

--- libxml2/seed.sh
+++ libxml2/seed.sh
@@ -14,20 +14,20 @@ _setup()
 
 _build()
 {
-	./configure --prefix=$FAKEROOT/$NAME/usr \
+	./configure --prefix=/usr \
             --sysconfdir=/etc       \
             --disable-static        \
             --with-history          \
             --with-icu              \
             PYTHON=/usr/bin/python3 \
-            --docdir=$FAKEROOT/$NAME/usr/share/doc/libxml2-${VERSION}
+            --docdir=/usr/share/doc/libxml2-${VERSION}
 
 	make -j${BUILD_JOBS}
 }
 
 _install()
 {
-	make install
+	make DESTDIR=$FAKEROOT/$NAME install
 }
 
 #_build32()
