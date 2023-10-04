265429e4402e94507ec6ed7a5598f9ca7bb971e6
Toasterbirb <git@toasterbirb.com>
2023-10-04 23:53:04 +0300

    Revert "Attempt to install libxml2 python bindings with pip"
    This reverts commit f5b4b4ecc1211c8bcfaf57ba4c8d2c2b11266023.

--- libxml2/seed.sh
+++ libxml2/seed.sh
@@ -4,7 +4,7 @@ VERSION="2.10.4"
 SOURCE="https://download.gnome.org/sources/libxml2/$(echo "$VERSION" | awk -F '.' '{print $1 "." $2}')/libxml2-${VERSION}.tar.xz"
 CHECKSUM="76808c467a58c31e2dbd511e71d5fd13"
 DEPS="icu"
-FLAGS="important python"
+FLAGS="important"
 
 _setup()
 {
@@ -24,20 +24,11 @@ _build()
             --docdir=/usr/share/doc/libxml2-${VERSION}
 
 	make -j${BUILD_JOBS}
-
-	# Build the python module
-	cd python
-	pip3 wheel -w $FAKEROOT/$NAME/$PYTHON_DIST --no-build-isolation --no-deps $PWD
-	cd ..
 }
 
 _install()
 {
 	make DESTDIR=$FAKEROOT/$NAME install
-
-	# Install the python module
-	cd python
-	pip3 install --no-index --find-links=$FAKEROOT/$NAME/$PYTHON_DIST wheel
 }
 
 #_build32()
