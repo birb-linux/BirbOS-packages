c9eed4ae5d1e77fb1a20cef1a080668b75c9f4a0
Toasterbirb <git@toasterbirb.com>
2023-10-16 21:32:05 +0300

    Bump dwm version

--- dwm/seed.sh
+++ dwm/seed.sh
@@ -1,8 +1,8 @@
 NAME="dwm"
 DESC="Dynamic tiling window manager"
-VERSION="2.0.0"
+VERSION="2.1.0"
 SOURCE="https://github.com/Toasterbirb/dwm/archive/refs/tags/${VERSION}.tar.gz"
-CHECKSUM="57d367385f9653dbaedd1d1c5680ae76"
+CHECKSUM="37d9b678c0d8aae1d3841718efb9eb2b"
 DEPS="dmenu xorg-apps xorg-libs xorg-server pkg-config ncurses fontconfig mononoki xf86-input-libinput xf86-video-fbdev"
 FLAGS=""
 
@@ -14,7 +14,21 @@ _setup()
 
 _build()
 {
-	make -j${BUILD_JOBS}
+	# If the system configs exist, build against them
+	if [ -f /etc/dwm/config.h ] && [ -f /etc/dwm/theme.h ]
+	then
+		make SETTINGS="-DSYSTEM_CONFIG" -j${BUILD_JOBS}
+	else
+		# Do a normal build
+		make -j${BUILD_JOBS}
+
+		# Copy the config files over if they cant be found in /etc
+		mkdir -p /etc/dwm
+		for i in config.h theme.h
+		do
+			[ ! -f /etc/dwm/$i ] && mv $i /etc/dwm/$i
+		done
+	fi
 }
 
 _install()
