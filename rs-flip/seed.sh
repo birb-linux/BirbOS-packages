0774de0b7fe4b010ba0d9007ea1f3dff5706a440
Toasterbirb <git@toasterbirb.com>
2023-11-02 19:22:05 +0200

    Bump rs-flip version

--- rs-flip/seed.sh
+++ rs-flip/seed.sh
@@ -1,8 +1,8 @@
 NAME="rs-flip"
 DESC="RuneScape flipping utility to make GE standing more profitable"
-VERSION="1.1.1"
+VERSION="1.2.0"
 SOURCE="https://github.com/Toasterbirb/RS-Flip/archive/refs/tags/${VERSION}.tar.gz"
-CHECKSUM="a7a1609e96385eb19526294fbf9324fd"
+CHECKSUM="739cb070fbde1a919c37156d4dff5020"
 DEPS="cmake"
 FLAGS=""
 
