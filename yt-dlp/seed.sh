f25c1c3a3e300a0e0985f1f9496e3f4a5854c0c7
Toasterbirb <git@toasterbirb.com>
2023-11-03 00:46:55 +0200

    Bump yt-dlp version

--- yt-dlp/seed.sh
+++ yt-dlp/seed.sh
@@ -1,8 +1,8 @@
 NAME="yt-dlp"
 DESC="A youtube-dl fork with additional features and fixes"
-VERSION="2023.07.06"
+VERSION="2023.10.13"
 SOURCE="https://github.com/yt-dlp/yt-dlp/releases/download/${VERSION}/yt-dlp.tar.gz"
-CHECKSUM="20ff3cad19bda18fa3bf9e8492222645"
+CHECKSUM="91b9c05e543be09278e189f9348b05db"
 DEPS="ffmpeg"
 FLAGS=""
 
