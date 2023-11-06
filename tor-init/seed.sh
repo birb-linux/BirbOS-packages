424f0cf31b8c8534151954d11dbee20b7e9a9189
Toasterbirb <git@toasterbirb.com>
2023-11-06 16:58:15 +0200

    Bump tor-init version

--- tor-init/seed.sh
+++ tor-init/seed.sh
@@ -1,8 +1,8 @@
 NAME="tor-init"
 DESC="SysVinit bootscripts for BirbOS"
-VERSION="1.0.1"
+VERSION="1.0.2"
 SOURCE="http://birbgitfh224rep6tmdofmr6qlo6wx43umqzt3hjubnncr55sdlfmtad.onion/birb-linux/bootscripts/archive/${VERSION}.tar.gz"
-CHECKSUM="eb175e5076e9d774ac85fac3c8aaf5c4"
+CHECKSUM="8c52df0a5dc09e7683756285ec050323"
 DEPS="tor"
 FLAGS=""
 
