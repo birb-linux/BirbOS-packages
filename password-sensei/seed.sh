31030ee1889d383e9af6bf0aa685d3b729264f0e
Toasterbirb <git@toasterbirb.com>
2023-11-07 19:42:22 +0200

    Package password-sensei

---
+++ password-sensei/seed.sh
@@ -0,0 +1,23 @@
+NAME="password-sensei"
+DESC="A password learning tool that asks for random account passwords and compares them to stored hashes"
+VERSION="1.0.0"
+SOURCE="http://birbgitfh224rep6tmdofmr6qlo6wx43umqzt3hjubnncr55sdlfmtad.onion/toasterbirb/password-sensei/archive/${VERSION}.tar.gz"
+CHECKSUM="4a240b78ffa53fcad92a325ab57fe6be"
+DEPS=""
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}
+}
+
+_build()
+{
+	printf ""
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME install
+}
