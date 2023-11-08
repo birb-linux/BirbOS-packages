c1cb39d538775ec1273fb1631c650bc53e94ef07
Toasterbirb <git@toasterbirb.com>
2023-11-08 10:44:22 +0200

    Package xf86-input-wacom

---
+++ xf86-input-wacom/seed.sh
@@ -0,0 +1,26 @@
+NAME="xf86-input-wacom"
+DESC="X.Org X11 driver and SDK for Wacom and Wacom-like tablets"
+VERSION="1.2.0"
+SOURCE="https://github.com/linuxwacom/xf86-input-wacom/releases/download/xf86-input-wacom-${VERSION}/xf86-input-wacom-${VERSION}.tar.bz2"
+CHECKSUM="9de66dbfa7dbb3897708d35776da0108"
+DEPS="xorg-server"
+FLAGS=""
+
+_setup()
+{
+	kernel_check "HID_SUPPORT HID HID_WACOM USB_HID USB_SUPPORT USB"
+
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=/usr --with-systemd-unit-dir=no
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME install
+}
