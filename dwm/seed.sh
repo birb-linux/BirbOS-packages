1958a9665f3b09a82ac0bdea2fb65fe57e7a72d6
Toasterbirb <git@toasterbirb.com>
2023-11-15 18:43:38 +0200

    Bump dwm version

--- dwm/seed.sh
+++ dwm/seed.sh
@@ -1,15 +1,15 @@
 NAME="dwm"
 DESC="Dynamic tiling window manager"
-VERSION="2.1.0"
-SOURCE="https://github.com/Toasterbirb/dwm/archive/refs/tags/${VERSION}.tar.gz"
-CHECKSUM="37d9b678c0d8aae1d3841718efb9eb2b"
+VERSION="2.1.1"
+SOURCE="http://birbgitfh224rep6tmdofmr6qlo6wx43umqzt3hjubnncr55sdlfmtad.onion/toasterbirb/dwm/archive/${VERSION}.tar.gz"
+CHECKSUM="6f379651cb4f171dc9e33becf1a4fc68"
 DEPS="dmenu xorg-apps xorg-libs xorg-server pkg-config ncurses fontconfig mononoki xf86-input-libinput xf86-video-fbdev"
 FLAGS=""
 
 _setup()
 {
 	tar -xf $DISTFILES/$(basename $SOURCE)
-	cd ${NAME}-${VERSION}
+	cd ${NAME}
 }
 
 _build()
