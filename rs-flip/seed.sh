c810f094ee2ddceebf6e918aab799622a1116d2a
Toasterbirb <git@toasterbirb.com>
2023-11-16 14:54:22 +0200

    Fix the checksum

--- rs-flip/seed.sh
+++ rs-flip/seed.sh
@@ -2,7 +2,7 @@ NAME="rs-flip"
 DESC="RuneScape flipping utility to make GE standing more profitable"
 VERSION="1.2.1"
 SOURCE="http://birbgitfh224rep6tmdofmr6qlo6wx43umqzt3hjubnncr55sdlfmtad.onion/toasterbirb/RS-Flip/archive/${VERSION}.tar.gz"
-CHECKSUM="e8d56bc54621037842ee9f0aeae27746"
+CHECKSUM="86a79ee550a2037d8c623ddb6a72b3b7"
 DEPS="cmake json-nlohmann doctest"
 FLAGS=""
 
