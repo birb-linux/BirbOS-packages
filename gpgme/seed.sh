2cd4bc15bbfafd10bd8ce5ed54a4f7cc9dbea557
Toasterbirb <git@toasterbirb.com>
2023-11-21 01:18:54 +0200

    Package gpgme
    Fixes #32

---
+++ gpgme/seed.sh
@@ -0,0 +1,24 @@
+NAME="gpgme"
+DESC="C library that allows cryptography support to be added to a program"
+VERSION="1.21.0"
+SOURCE="https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-${VERSION}.tar.bz2"
+CHECKSUM="3a83288e753a7da53a5626d29e4416fd"
+DEPS="libassuan"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	./configure --prefix=/usr --disable-gpg-test
+	make -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME PREFIX=/usr install
+}
