2f5edd1988d628748bc1bc42ea59eac27d4e7eb6
Toasterbirb <git@toasterbirb.com>
2023-11-21 01:35:17 +0200

    Package conmon

---
+++ conmon/seed.sh
@@ -0,0 +1,23 @@
+NAME="conmon"
+DESC="An OCI container runtime monitor"
+VERSION="2.1.8"
+SOURCE="https://github.com/containers/conmon/archive/refs/tags/v${VERSION}.tar.gz"
+CHECKSUM="753a2d554c6b5c55c93f069a20d9ebf9"
+DEPS="libseccomp pkg-config"
+FLAGS=""
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	make PREFIX=/usr -j${BUILD_JOBS}
+}
+
+_install()
+{
+	make DESTDIR=$FAKEROOT/$NAME PREFIX=/usr install
+}
