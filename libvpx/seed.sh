87f1862ac5936258200fa7c29af21ba2d42da10c
Toasterbirb <git@toasterbirb.com>
2023-12-18 16:51:29 +0200

    Bump libvpx version due to a security issue

--- libvpx/seed.sh
+++ libvpx/seed.sh
@@ -1,8 +1,8 @@
 NAME="libvpx"
 DESC="Reference implementations of the VP8 Codec"
-VERSION="1.13.0"
+VERSION="1.13.1"
 SOURCE="https://github.com/webmproject/libvpx/archive/v${VERSION}/libvpx-${VERSION}.tar.gz"
-CHECKSUM="d5fd45a806a65a57d6635f9e7a98a1b2"
+CHECKSUM="d2c9de1c247f18a204e75ecefa7a2217"
 DEPS="nasm which"
 FLAGS=""
 
