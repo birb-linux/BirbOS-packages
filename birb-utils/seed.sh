89fbb986ebd3ca03632223dd24fd568ef482f30a
Toasterbirb <git@toasterbirb.com>
2024-05-03 16:06:48 +0300

    Fix the checksum

--- birb-utils/seed.sh
+++ birb-utils/seed.sh
@@ -2,7 +2,7 @@ NAME="birb-utils"
 DESC="Miscellaneous utility scripts and programs made for BirbOS"
 VERSION="0.7.1"
 SOURCE="https://github.com/Toasterbirb/birb-utils/archive/refs/tags/${VERSION}.tar.gz"
-CHECKSUM="2c819117e7ff3dc030fbe741fbbb64e5"
+CHECKSUM="8379b5ead39e44b7bf8b3a2e11f1c4c5"
 DEPS="file wget curl sudo gnupg"
 FLAGS=""
 
