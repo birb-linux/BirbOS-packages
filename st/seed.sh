51622307354b1f6f9288c030316cf2dececf7094
Toasterbirb <git@toasterbirb.com>
2024-05-03 15:21:31 +0300

    Bump st version

--- st/seed.sh
+++ st/seed.sh
@@ -1,8 +1,8 @@
 NAME="st"
 DESC="Simple terminal for X"
-VERSION="1.0.0"
+VERSION="1.1.0"
 SOURCE="https://github.com/Toasterbirb/st/archive/refs/tags/${VERSION}.tar.gz"
-CHECKSUM="2d07cf956aa8cf2f2c63925605c5aed7"
+CHECKSUM="ecd1446b56011ba180ec619f0ed5de1e"
 DEPS="xorg-libs pkg-config fontconfig mononoki freetype harfbuzz graphite2 zlib glib pcre2"
 FLAGS=""
 
