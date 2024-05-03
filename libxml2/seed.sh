1888ced8936aedc4193bbdedf6fdd4a035dbe4ba
Toasterbirb <git@toasterbirb.com>
2024-05-03 17:09:13 +0300

    Version bump: libxml2

--- libxml2/seed.sh
+++ libxml2/seed.sh
@@ -1,8 +1,8 @@
 NAME="libxml2"
 DESC="Libraries and utilities for parsing XML files"
-VERSION="2.10.4"
+VERSION="2.12.6"
 SOURCE="https://download.gnome.org/sources/libxml2/$(echo "$VERSION" | awk -F '.' '{print $1 "." $2}')/libxml2-${VERSION}.tar.xz"
-CHECKSUM="76808c467a58c31e2dbd511e71d5fd13"
+CHECKSUM="37fab9ace78d8a085af81dc9a19f36d9"
 DEPS="icu"
 FLAGS="important"
 
