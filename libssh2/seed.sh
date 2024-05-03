c6592a95a535f5f1be76e1dd31b2b2b27a65d4fd
Toasterbirb <git@toasterbirb.com>
2024-05-03 17:40:43 +0300

    Update the libssh2 blfs patch

--- libssh2/seed.sh
+++ libssh2/seed.sh
@@ -15,7 +15,7 @@ _setup()
 _build()
 {
 	# Fix an incompatibility issue with recent OpenSSH versions
-	patch -Np1 -i $PKG_PATH/libssh2-1.10.0-upstream_fix-1.patch
+	patch -Np1 -i $PKG_PATH/libssh2-1.11.0-security_fixes-1.patch
 
 	./configure --prefix=$FAKEROOT/$NAME/usr --disable-static
 	make -j${BUILD_JOBS}
