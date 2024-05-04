d8b1d0a55fee36d4a70e1b329e72fbb972e1833f
Toasterbirb <git@toasterbirb.com>
2024-05-04 17:13:44 +0300

    Version bump: libdrm

--- libdrm/seed.sh
+++ libdrm/seed.sh
@@ -1,8 +1,8 @@
 NAME="libdrm"
 DESC="Userspace library for accessing the direct rendering manager (DRM)"
-VERSION="2.4.115"
+VERSION="2.4.120"
 SOURCE="https://dri.freedesktop.org/libdrm/libdrm-${VERSION}.tar.xz"
-CHECKSUM="5403981a20c964f4c893ff91393652bd"
+CHECKSUM="f8521de37ccbd15987b843a83fabd567"
 DEPS="xorg-libs meson ninja"
 FLAGS="32bit"
 
