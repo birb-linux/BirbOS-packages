65e592d457da585a250848b3665ce211fa7ead18
Toasterbirb <git@toasterbirb.com>
2024-05-04 16:20:39 +0300

    Version bump: birb-utils

--- birb-utils/seed.sh
+++ birb-utils/seed.sh
@@ -1,8 +1,8 @@
 NAME="birb-utils"
 DESC="Miscellaneous utility scripts and programs made for BirbOS"
-VERSION="0.8.0"
+VERSION="0.8.1"
 SOURCE="https://github.com/Toasterbirb/birb-utils/archive/refs/tags/${VERSION}.tar.gz"
-CHECKSUM="740153a820fbe084f12451f2a7e0e61a"
+CHECKSUM="b32cb9c32978d26d837173cf154a3d19"
 DEPS="file wget curl sudo gnupg fzf"
 FLAGS=""
 
