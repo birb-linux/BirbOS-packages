519ac1b2d51d2c8498534ecae88b3bc421132f01
Toasterbirb <git@toasterbirb.com>
2024-05-04 18:36:59 +0300

    Version bump: gparted

--- gparted/seed.sh
+++ gparted/seed.sh
@@ -1,8 +1,8 @@
 NAME="gparted"
 DESC="Gnome Partition Editor, a Gtk 3 GUI for other command line tools that can create, reorganise or delete disk partitions"
-VERSION="1.5.0"
+VERSION="1.6.0"
 SOURCE="https://downloads.sourceforge.net/gparted/gparted-${VERSION}.tar.gz"
-CHECKSUM="9adbd4b1cbcb7a7c76dcc0e9ffed9a7c"
+CHECKSUM="b2006a0a3f35853e7d7dc34c87db11f2"
 DEPS="gtkmm parted"
 FLAGS="test"
 
