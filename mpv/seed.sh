ce09ad8a5f861a2d379543449f983e6faee01e2f
Toasterbirb <git@toasterbirb.com>
2024-05-04 16:27:26 +0300

    Add a missing dependency

--- mpv/seed.sh
+++ mpv/seed.sh
@@ -3,7 +3,7 @@ DESC="Command line video player"
 VERSION="0.37.0"
 SOURCE="https://github.com/mpv-player/mpv/archive/refs/tags/v${VERSION}.tar.gz"
 CHECKSUM="54bd6864cc831f1fee6dee693a0421eb"
-DEPS="xorg-libs pulseaudio ffmpeg zlib libass libjpeg-turbo uchardet libxpresent luajit meson"
+DEPS="xorg-libs pulseaudio ffmpeg zlib libass libjpeg-turbo uchardet libxpresent luajit meson libplacebo"
 FLAGS=""
 
 _setup()
