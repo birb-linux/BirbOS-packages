7fda5840fa364850faac78d08698b901fc404489
Toasterbirb <git@toasterbirb.com>
2024-05-04 17:38:52 +0300

    Version bump: libpcap

--- libpcap/seed.sh
+++ libpcap/seed.sh
@@ -1,8 +1,8 @@
 NAME="libpcap"
 DESC="Functions for user-level packet capture"
-VERSION="1.10.3"
+VERSION="1.10.4"
 SOURCE="https://www.tcpdump.org/release/libpcap-${VERSION}.tar.gz"
-CHECKSUM="c42fde2eb75a5075f19116f8c9768573"
+CHECKSUM="0322e28dd76cda8066bb6d00fee5969b"
 DEPS=""
 FLAGS=""
 
@@ -14,7 +14,7 @@ _setup()
 
 _build()
 {
-	./configure --prefix=$FAKEROOT/$NAME/usr
+	./configure --prefix=/usr
 
 	make -j${BUILD_JOBS}
 }
@@ -24,5 +24,5 @@ _install()
 	# Skip installing the static libraries
 	sed -i '/INSTALL_DATA.*libpcap.a\|RANLIB.*libpcap.a/ s/^/#/' Makefile
 
-	make install
+	make DESTDIR=$FAKEROOT/$NAME install
 }
