991e9148893d36a1473a4d67fcbe7872a7b4d1d6
Toasterbirb <git@toasterbirb.com>
2024-05-04 16:50:11 +0300

    Package libplacebo

---
+++ libplacebo/seed.sh
@@ -0,0 +1,37 @@
+NAME="libplacebo"
+DESC="Library for processing image and video primitives and shaders"
+VERSION="6.338.2"
+SOURCE="https://github.com/haasn/libplacebo/archive/refs/tags/v${VERSION}/libplacebo-${VERSION}.tar.gz"
+CHECKSUM="4f5b743ec43c684e6da0d56efed3891c"
+DEPS="ffmpeg glad glslang meson"
+FLAGS="test"
+
+_setup()
+{
+	tar -xf $DISTFILES/$(basename $SOURCE)
+	cd ${NAME}-${VERSION}
+}
+
+_build()
+{
+	mkdir build
+	cd    build
+
+	meson setup ..            \
+		  --prefix=/usr       \
+		  --buildtype=release \
+		  -Dtests=true        \
+		  -Ddemos=false
+
+	ninja
+}
+
+_install()
+{
+	DESTDIR=$FAKEROOT/$NAME ninja install
+}
+
+_test()
+{
+	ninja test
+}
