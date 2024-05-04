98604e9a5bd30221544ddad65af3093404d98ebd
Toasterbirb <git@toasterbirb.com>
2024-05-04 17:49:22 +0300

    Version bump: libnotify

--- libnotify/seed.sh
+++ libnotify/seed.sh
@@ -1,9 +1,9 @@
 NAME="libnotify"
 DESC="The libnotify library is used to send desktop notifications to a notification daemon"
-VERSION="0.8.1"
+VERSION="0.8.3"
 SHORT_VERSION="$(short_version $VERSION)"
 SOURCE="https://download.gnome.org/sources/libnotify/${SHORT_VERSION}/libnotify-${VERSION}.tar.xz"
-CHECKSUM="1495f279b255b4493ac3588559823158"
+CHECKSUM="952e55f45067b6a5b9eab4310c9cd658"
 DEPS="gtk+ notification-daemon meson ninja"
 FLAGS=""
 
