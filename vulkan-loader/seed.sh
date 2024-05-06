be9119094647220eb67e15c424308bc0617808a4
Toasterbirb <git@toasterbirb.com>
2024-05-06 03:13:43 +0300

    Build 32bit version of vulkan-loader

--- vulkan-loader/seed.sh
+++ vulkan-loader/seed.sh
@@ -5,7 +5,7 @@ VERSION="1.3.283"
 SOURCE="https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/v${VERSION}.tar.gz"
 CHECKSUM="b33487f0426dbfffbdc0b2118e3b3e6c"
 DEPS="libx11 libxcb libxkbcommon wayland libxrandr"
-FLAGS=""
+FLAGS="32bit"
 
 _setup()
 {
@@ -26,4 +26,28 @@ _build()
 _install()
 {
 	make DESTDIR="$FAKEROOT/$NAME" install
+}
+
+_build32()
+{
+	cd ..
+	rm -r build
+	mkdir build
+	cd build || exit 1
+
+	ASFLAGS="--32" LDFLAGS="-L/usr/lib32" CC="gcc -m32" CXX="g++ -m32" cmake \
+		-DUPDATE_DEPS=ON \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DUSE_GAS=OFF \
+		..
+
+	make -j "${BUILD_JOBS}"
+}
+
+_install32()
+{
+	make DESTDIR=$PWD/DESTDIR install
+	cp -Rv DESTDIR/usr/lib/* "$FAKEROOT/$NAME/usr/lib32"
+	rm -rf DESTDIR
 }
