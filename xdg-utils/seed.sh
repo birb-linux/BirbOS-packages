94d12b4c3ab36200ddae55360aa811e03a9d59e5
Toasterbirb <git@toasterbirb.com>
2024-05-06 16:19:16 +0300

    Package xdg-utils

---
+++ xdg-utils/seed.sh
@@ -0,0 +1,25 @@
+# shellcheck disable=SC2034
+NAME="xdg-utils"
+DESC="Set of command line tools that assist applications with a variety of desktop integration tasks"
+VERSION="1.2.1"
+SOURCE="https://gitlab.freedesktop.org/xdg/xdg-utils/-/archive/v${VERSION}/xdg-utils-v${VERSION}.tar.gz"
+CHECKSUM="4c72585a98ba8f775cb9e72b066cc0df"
+DEPS="xmlto links xorg-apps"
+FLAGS=""
+
+_setup()
+{
+	tar -xf "$DISTFILES/$(basename $SOURCE)"
+	cd "${NAME}-${VERSION}" || exit 1
+}
+
+_build()
+{
+	./configure --prefix=/usr
+	make -j "${BUILD_JOBS}"
+}
+
+_install()
+{
+	make DESTDIR="$FAKEROOT/$NAME" install
+}
