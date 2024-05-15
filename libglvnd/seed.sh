57218eb864c4448c81fb43bc947f368eca185bfc
Toasterbirb <git@toasterbirb.com>
2024-05-16 02:08:24 +0300

    Add a 32bit build to libglvnd

--- libglvnd/seed.sh
+++ libglvnd/seed.sh
@@ -5,7 +5,7 @@ VERSION="1.7.0"
 SOURCE="https://github.com/NVIDIA/libglvnd/archive/refs/tags/v${VERSION}.tar.gz"
 CHECKSUM="bbfdc3e1a2cd33a4a3d3a46498248376"
 DEPS="libxext libx11 xorgproto"
-FLAGS=""
+FLAGS="32bit"
 
 _setup()
 {
@@ -23,4 +23,23 @@ _build()
 _install()
 {
 	make DESTDIR="$FAKEROOT/$NAME" install
+}
+
+_build32()
+{
+	make distclean
+
+	LDFLAGS="-L/usr/lib32" CC="gcc -m32" ./configure \
+		--prefix=/usr \
+		--host=i686-pc-linux-gnu \
+		--libdir=/usr/lib32
+
+	make -j "${BUILD_JOBS}"
+}
+
+_install32()
+{
+	make DESTDIR="$PWD/DESTDIR" install
+	cp -Rv DESTDIR/usr/lib32/* "$FAKEROOT/$NAME/usr/lib32"
+	rm -rf DESTDIR
 }
